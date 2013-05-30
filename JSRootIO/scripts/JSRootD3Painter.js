264f8eddf02a6a63ab9cc893a969f526b81da746
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2013-05-30 11:31:34 +0200

    add a simple (one line) example of how to automatically display the first object in a file

--- JSRootIO/scripts/JSRootD3Painter.js
+++ JSRootIO/scripts/JSRootD3Painter.js
@@ -5417,6 +5417,8 @@ function createFillPatterns(svg, id, color) {
       }
       content += key_tree;
       $(container).append(content);
+      // to display the first object in the file, uncomment the following line
+      // setTimeout( function() { showObject(keys[0]['name'],keys[0]['cycle']); }, 20 );
    };
 
    JSROOTPainter.addDirectoryKeys = function(keys, container, dir_id) {
