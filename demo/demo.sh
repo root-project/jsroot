546e80251feaba21eef5e02023dc4850c51e2196
S.Linev <S.Linev@gsi.de>
2014-10-06 15:34:34 +0000

    Even more files generate by demo.sh
    git-svn-id: https://subversion.gsi.de/dabc/trunk/plugins/root/js@2592 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- demo/demo.sh
+++ demo/demo.sh
@@ -19,7 +19,10 @@ mkdir -p Canvases
 sed 's/\/jsrootsys/..\/../g' ../files/online.htm > Canvases/index.htm
 wget -nv $server/Canvases/h.json -O Canvases/h.json
 wget -nv $server/Canvases/h.xml -O Canvases/h.xml
-mkdir -p Canvases/c1; wget -nv $server/Canvases/c1/root.json.gz?compact=3 -O Canvases/c1/root.json.gz
+mkdir -p Canvases/c1 
+wget -nv $server/Canvases/c1/root.json.gz?compact=3 -O Canvases/c1/root.json.gz
+wget -nv -O Canvases/c1/root.png $server/Canvases/c1/root.png?w=400&h=300
+sed 's/\/jsrootsys/..\/..\/../g' ../files/draw.htm > Canvases/c1/draw.htm
 mkdir -p Files
 sed 's/\/jsrootsys/..\/../g' ../files/online.htm > Files/index.htm
 wget -nv $server/Files/h.json -O Files/h.json
@@ -28,9 +31,18 @@ mkdir -p Files/job1.root
 sed 's/\/jsrootsys/..\/..\/../g' ../files/online.htm > Files/job1.root/index.htm
 wget -nv $server/Files/job1.root/h.json -O Files/job1.root/h.json
 wget -nv $server/Files/job1.root/h.xml -O Files/job1.root/h.xml
-mkdir -p Files/job1.root/hpx; wget -nv $server/Files/job1.root/hpx/root.json.gz?compact=3 -O Files/job1.root/hpx/root.json.gz
-mkdir -p Files/job1.root/hpxpy; wget -nv $server/Files/job1.root/hpxpy/root.json.gz?compact=3 -O Files/job1.root/hpxpy/root.json.gz
-mkdir -p Files/job1.root/hprof; wget -nv $server/Files/job1.root/hprof/root.json.gz?compact=3 -O Files/job1.root/hprof/root.json.gz
+mkdir -p Files/job1.root/hpx
+wget -nv $server/Files/job1.root/hpx/root.json.gz?compact=3 -O Files/job1.root/hpx/root.json.gz
+wget -nv -O Files/job1.root/hpx/root.png $server/Files/job1.root/hpx/root.png?w=400&h=300
+sed 's/\/jsrootsys/..\/..\/..\/../g' ../files/draw.htm > Files/job1.root/hpx/draw.htm
+mkdir -p Files/job1.root/hpxpy 
+wget -nv $server/Files/job1.root/hpxpy/root.json.gz?compact=3 -O Files/job1.root/hpxpy/root.json.gz
+wget -nv -O Files/job1.root/hpxpy/root.png $server/Files/job1.root/hpxpy/root.png?w=400&h=300
+sed 's/\/jsrootsys/..\/..\/..\/../g' ../files/draw.htm > Files/job1.root/hpxpy/draw.htm
+mkdir -p Files/job1.root/hprof 
+wget -nv $server/Files/job1.root/hprof/root.json.gz?compact=3 -O Files/job1.root/hprof/root.json.gz
+wget -nv -O Files/job1.root/hprof/root.png $server/Files/job1.root/hprof/root.png?w=400&h=300 
+sed 's/\/jsrootsys/..\/..\/..\/../g' ../files/draw.htm > Files/job1.root/hprof/draw.htm
 
 # copy all files to web server
-# cp -rf index.htm h.xml h.json StreamerInfo Canvases Files ~/web-docs/js/3.0/demo
+cp -rf index.htm h.xml h.json StreamerInfo Canvases Files ~/web-docs/js/3.0/demo
