3112ffb882697e2f3f90e717e7b5c5917f90476d
S.Linev <S.Linev@gsi.de>
2014-10-06 15:15:27 +0000

    Copy more files for THttpServer demo
    git-svn-id: https://subversion.gsi.de/dabc/trunk/plugins/root/js@2591 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- demo/demo.sh
+++ demo/demo.sh
@@ -12,13 +12,25 @@ server=http://localhost:8080
 rm -rf index.htm h.xml h.json StreamerInfo Canvases Files
 
 sed 's/\/jsrootsys/../g' ../files/online.htm > index.htm
-wget $server/h.xml -O h.xml
-wget $server/h.json -O h.json
-mkdir -p StreamerInfo; wget $server/StreamerInfo/root.json.gz?compact=3 -O StreamerInfo/root.json.gz
-mkdir -p Canvases/c1; wget $server/Canvases/c1/root.json.gz?compact=3 -O Canvases/c1/root.json.gz
-mkdir -p Files/job1.root/hpx; wget $server/Files/job1.root/hpx/root.json.gz?compact=3 -O Files/job1.root/hpx/root.json.gz
-mkdir -p Files/job1.root/hpxpy; wget $server/Files/job1.root/hpxpy/root.json.gz?compact=3 -O Files/job1.root/hpxpy/root.json.gz
-mkdir -p Files/job1.root/hprof; wget $server/Files/job1.root/hprof/root.json.gz?compact=3 -O Files/job1.root/hprof/root.json.gz
+wget -nv $server/h.xml -O h.xml
+wget -nv $server/h.json -O h.json
+mkdir -p StreamerInfo; wget -nv $server/StreamerInfo/root.json.gz?compact=3 -O StreamerInfo/root.json.gz
+mkdir -p Canvases 
+sed 's/\/jsrootsys/..\/../g' ../files/online.htm > Canvases/index.htm
+wget -nv $server/Canvases/h.json -O Canvases/h.json
+wget -nv $server/Canvases/h.xml -O Canvases/h.xml
+mkdir -p Canvases/c1; wget -nv $server/Canvases/c1/root.json.gz?compact=3 -O Canvases/c1/root.json.gz
+mkdir -p Files
+sed 's/\/jsrootsys/..\/../g' ../files/online.htm > Files/index.htm
+wget -nv $server/Files/h.json -O Files/h.json
+wget -nv $server/Files/h.xml -O Files/h.xml
+mkdir -p Files/job1.root
+sed 's/\/jsrootsys/..\/..\/../g' ../files/online.htm > Files/job1.root/index.htm
+wget -nv $server/Files/job1.root/h.json -O Files/job1.root/h.json
+wget -nv $server/Files/job1.root/h.xml -O Files/job1.root/h.xml
+mkdir -p Files/job1.root/hpx; wget -nv $server/Files/job1.root/hpx/root.json.gz?compact=3 -O Files/job1.root/hpx/root.json.gz
+mkdir -p Files/job1.root/hpxpy; wget -nv $server/Files/job1.root/hpxpy/root.json.gz?compact=3 -O Files/job1.root/hpxpy/root.json.gz
+mkdir -p Files/job1.root/hprof; wget -nv $server/Files/job1.root/hprof/root.json.gz?compact=3 -O Files/job1.root/hprof/root.json.gz
 
 # copy all files to web server
-cp -rf index.htm h.xml h.json StreamerInfo Canvases Files ~/web-docs/js/3.0/demo
+# cp -rf index.htm h.xml h.json StreamerInfo Canvases Files ~/web-docs/js/3.0/demo
