4c48aec4de46dfabef16eb601670101bd89d422f
S.Linev <S.Linev@gsi.de>
2014-12-08 14:03:30 +0000

    simplify close of collapsible element
    git-svn-id: https://subversion.gsi.de/dabc/trunk/plugins/root/js@2845 bcbf6573-9a26-0410-9ebc-ce4ab7aade96

--- scripts/JSRootPainter.js
+++ scripts/JSRootPainter.js
@@ -7830,17 +7830,6 @@
 
    // ==================================================
 
-   JSROOT.CloseCollapsible = function(e, el) {
-      var sel = $(el)[0].textContent;
-      if (typeof (sel) == 'undefined')
-         return;
-      sel.replace(' x', '');
-      sel.replace(';', '');
-      sel.replace(' ', '');
-      $(el).next().andSelf().remove();
-      e.stopPropagation();
-   };
-
    JSROOT.CollapsibleDisplay = function(frameid) {
       JSROOT.MDIDisplay.call(this, frameid);
       this.cnt = 0; // use to count newly created frames
@@ -7891,8 +7880,9 @@
             .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
             .hover(function() { $(this).toggleClass("ui-state-hover"); })
             .prepend('<span class="ui-icon ui-icon-triangle-1-e"></span>')
-            .append('<button type="button" class="closeButton" title="close canvas" onclick="JSROOT.CloseCollapsible(event, \'#'
-                        + uid + '\')"><img class="img_remove" src="" alt=""/></button>')
+            .append('<button type="button" class="closeButton" title="close canvas" '+ 
+                    'onclick="javascript: $(this).parent().next().andSelf().remove();">'+
+                    '<img class="img_remove" src="" alt=""/></button>')
             .click( function() {
                      $(this).toggleClass("ui-accordion-header-active ui-state-active ui-state-default ui-corner-bottom")
                            .find("> .ui-icon").toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s")
