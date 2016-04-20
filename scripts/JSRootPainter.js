25e0825675854319c766b1286543fde73737825a
Sergey Linev <S.Linev@gsi.de>
2016-04-20 12:42:02 +0200

    Fix - draw axis with no ticks

--- scripts/JSRootPainter.js
+++ scripts/JSRootPainter.js
@@ -4026,7 +4026,8 @@
          lasth = h2;
       }
 
-      axis_g.append("svg:path").attr("d", res).style("stroke", axis_color);
+      if (res.length > 0)
+         axis_g.append("svg:path").attr("d", res).style("stroke", axis_color);
 
       var last = vertical ? h : 0,
           labelfont = JSROOT.Painter.getFontDetails(axis.fLabelFont, Math.round(axis.fLabelSize * (is_gaxis ? this.pad_height() : h))),
