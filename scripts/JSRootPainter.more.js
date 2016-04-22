5494a3f0c0ed83feb315e452b0a840b2121707c2
Sergey Linev <S.Linev@gsi.de>
2016-04-22 15:59:09 +0200

    Access draw attributes of ellipse

--- scripts/JSRootPainter.more.js
+++ scripts/JSRootPainter.more.js
@@ -553,49 +553,45 @@
 
    JSROOT.Painter.drawEllipse = function(divid, obj, opt) {
 
-      this.ellipse = obj;
+      this.SetObject(obj);
       this.SetDivId(divid);
 
-      // function used for live update of object
-      this['UpdateObject'] = function(obj) {
-         // copy all fields
-         JSROOT.extend(this.ellipse, obj);
-      }
+      this.Redraw = function() {
+         var ellipse = this.GetObject();
 
-      this['Redraw'] = function() {
-         var lineatt = JSROOT.Painter.createAttLine(this.ellipse);
-         var fillatt = this.createAttFill(this.ellipse);
+         if(!this.lineatt) this.lineatt = JSROOT.Painter.createAttLine(ellipse);
+         if (!this.fillatt) this.fillatt = this.createAttFill(ellipse);
 
          // create svg:g container for ellipse drawing
          this.RecreateDrawG(this.main_painter() == null);
 
-         var x = this.AxisToSvg("x", this.ellipse.fX1);
-         var y = this.AxisToSvg("y", this.ellipse.fY1);
-         var rx = this.AxisToSvg("x", this.ellipse.fX1 + this.ellipse.fR1) - x;
-         var ry = y - this.AxisToSvg("y", this.ellipse.fY1 + this.ellipse.fR2);
+         var x = this.AxisToSvg("x", ellipse.fX1);
+         var y = this.AxisToSvg("y", ellipse.fY1);
+         var rx = this.AxisToSvg("x", ellipse.fX1 + ellipse.fR1) - x;
+         var ry = y - this.AxisToSvg("y", ellipse.fY1 + ellipse.fR2);
 
-         if ((this.ellipse.fPhimin == 0) && (this.ellipse.fPhimax == 360) && (this.ellipse.fTheta == 0)) {
+         if ((ellipse.fPhimin == 0) && (ellipse.fPhimax == 360) && (ellipse.fTheta == 0)) {
             // this is simple case, which could be drawn with svg:ellipse
             this.draw_g
                 .append("svg:ellipse")
                 .attr("cx", x.toFixed(1)).attr("cy", y.toFixed(1))
                 .attr("rx", rx.toFixed(1)).attr("ry", ry.toFixed(1))
-                .call(lineatt.func).call(fillatt.func);
+                .call(this.lineatt.func).call(this.fillatt.func);
             return;
          }
 
          // here svg:path is used to draw more complex figure
 
-         var ct = Math.cos(Math.PI*this.ellipse.fTheta/180.);
-         var st = Math.sin(Math.PI*this.ellipse.fTheta/180.);
+         var ct = Math.cos(Math.PI*ellipse.fTheta/180.);
+         var st = Math.sin(Math.PI*ellipse.fTheta/180.);
 
-         var dx1 =  rx * Math.cos(this.ellipse.fPhimin*Math.PI/180.);
-         var dy1 =  ry * Math.sin(this.ellipse.fPhimin*Math.PI/180.);
+         var dx1 =  rx * Math.cos(ellipse.fPhimin*Math.PI/180.);
+         var dy1 =  ry * Math.sin(ellipse.fPhimin*Math.PI/180.);
          var x1 =  dx1*ct - dy1*st;
          var y1 = -dx1*st - dy1*ct;
 
-         var dx2 = rx * Math.cos(this.ellipse.fPhimax*Math.PI/180.);
-         var dy2 = ry * Math.sin(this.ellipse.fPhimax*Math.PI/180.);
+         var dx2 = rx * Math.cos(ellipse.fPhimax*Math.PI/180.);
+         var dy2 = ry * Math.sin(ellipse.fPhimax*Math.PI/180.);
          var x2 =  dx2*ct - dy2*st;
          var y2 = -dx2*st - dy2*ct;
 
@@ -604,9 +600,9 @@
             .append("svg:path")
             .attr("d", "M 0,0" +
                        " L " + x1.toFixed(1) + "," + y1.toFixed(1) +
-                       " A " + rx.toFixed(1) + " " + ry.toFixed(1) + " " + -this.ellipse.fTheta.toFixed(1) + " 1 0 " + x2.toFixed(1) + "," + y2.toFixed(1) +
+                       " A " + rx.toFixed(1) + " " + ry.toFixed(1) + " " + -ellipse.fTheta.toFixed(1) + " 1 0 " + x2.toFixed(1) + "," + y2.toFixed(1) +
                        " L 0,0 Z")
-            .call(lineatt.func).call(fillatt.func);
+            .call(this.lineatt.func).call(this.fillatt.func);
       }
 
       this.Redraw(); // actual drawing
