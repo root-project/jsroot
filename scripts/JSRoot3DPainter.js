164c7399140a2ca8df4babae69dcc4932f2132e1
Sergey Linev <S.Linev@gsi.de>
2016-09-14 10:38:33 +0200

    Remove duplicated points from grids

--- scripts/JSRoot3DPainter.js
+++ scripts/JSRoot3DPainter.js
@@ -1445,7 +1445,7 @@
       if ((handle.i2 - handle.i1 < 2) || (handle.j2 - handle.j1 < 2)) return;
 
       // get levels
-      var levels = null, dolines = true, docolorfaces = false;
+      var levels = null, dolines = true, docolorfaces = false, dogrid = false;
 
       // var zticks = main.z_handle.CreateTicks(true);
       // console.log('zticks', zticks);
@@ -1453,7 +1453,7 @@
       switch(this.options.Surf) {
          case 11: levels = this.GetContour(); docolorfaces = true; break;
          case 12: levels = this.GetContour(); docolorfaces = true; dolines = false; break;
-         default: levels = main.z_handle.CreateTicks(true);  break; // draw only lines
+         default: levels = main.z_handle.CreateTicks(true); dogrid = true; break; // draw only lines
       }
 
       var loop, nfaces = 0, nsegments = 0, ngridsegments = 0,
@@ -1482,10 +1482,9 @@
       }
 
       var pntbuf = new Float32Array(6*3), k = 0, lastpart = 0; // maximal 6 points
-      var gridpnts = [ new Float32Array(2*3), new Float32Array(2*3) ];
-      var gridcnt = [0, 0];
+      var gridpnts = new Float32Array(2*3), gridcnt = 0;
 
-      function AddCrossingPoint(xx1,yy1,zz1, xx2,yy2,zz2, lindx, gridi) {
+      function AddCrossingPoint(xx1,yy1,zz1, xx2,yy2,zz2, lindx, with_grid) {
          if (k>=pntbuf.length) console.log('more than 6 points???');
 
          var part = (levels[lindx] - zz1) / (zz2 - zz1), shift = 3;
@@ -1501,11 +1500,12 @@
          pntbuf[k+1] = yy1 + part*(yy2-yy1);
          pntbuf[k+2] = levels[lindx];
 
-         var g = gridcnt[gridi];
-         gridpnts[gridi][g] = pntbuf[k];
-         gridpnts[gridi][g+1] = pntbuf[k+1];
-         gridpnts[gridi][g+2] = pntbuf[k+2];
-         gridcnt[gridi] = g+3;
+         if (with_grid && grid) {
+            gridpnts[gridcnt] = pntbuf[k];
+            gridpnts[gridcnt+1] = pntbuf[k+1];
+            gridpnts[gridcnt+2] = pntbuf[k+2];
+            gridcnt+=3;
+         }
 
          k += shift;
          lastpart = part;
@@ -1538,25 +1538,22 @@
                   nfaces++;
                }
 
-               var flag0 = 0, flag1 = 0; // if there crossing with correspondent level
+               var flag1 = 0; // if there crossing with correspondent level
                if (side1!==side2) {
-                  if ((side1<0) || (side2<0)) flag0 = 1;
                   if ((side1>0) || (side2>0)) flag1 = 1;
                }
                if (side2!==side3) {
-                  if ((side2<0) || (side3<0)) flag0 = 1;
                   if ((side2>0) || (side3>0)) flag1 = 1;
                }
                if (side3!==side1) {
-                  if ((side3<0) || (side1<0)) flag0 = 1;
                   if ((side3>0) || (side1>0)) flag1 = 1;
                }
-               ngridsegments += flag0 + flag1;
+               ngridsegments += flag1;
 
                continue;
             }
 
-            gridcnt[0] = gridcnt[1] = 0;
+            gridcnt = 0;
 
             k = 0;
             if (side1 === 0) { pntbuf[k] = x1; pntbuf[k+1] = y1; pntbuf[k+2] = z1; k+=3; }
@@ -1564,8 +1561,8 @@
             if (side1!==side2) {
                // order is important, should move from 1->2 point, checked via lastpart
                lastpart = 0;
-               if ((side1<0) || (side2<0)) AddCrossingPoint(x1,y1,z1, x2,y2,z2, lvl-1, 0);
-               if ((side1>0) || (side2>0)) AddCrossingPoint(x1,y1,z1, x2,y2,z2, lvl, 1);
+               if ((side1<0) || (side2<0)) AddCrossingPoint(x1,y1,z1, x2,y2,z2, lvl-1);
+               if ((side1>0) || (side2>0)) AddCrossingPoint(x1,y1,z1, x2,y2,z2, lvl, true);
             }
 
             if (side2 === 0) { pntbuf[k] = x2; pntbuf[k+1] = y2; pntbuf[k+2] = z2; k+=3; }
@@ -1573,8 +1570,8 @@
             if (side2!==side3) {
                // order is important, should move from 2->3 point, checked via lastpart
                lastpart = 0;
-               if ((side2<0) || (side3<0)) AddCrossingPoint(x2,y2,z2, x3,y3,z3, lvl-1, 0);
-               if ((side2>0) || (side3>0)) AddCrossingPoint(x2,y2,z2, x3,y3,z3, lvl, 1);
+               if ((side2<0) || (side3<0)) AddCrossingPoint(x2,y2,z2, x3,y3,z3, lvl-1);
+               if ((side2>0) || (side3>0)) AddCrossingPoint(x2,y2,z2, x3,y3,z3, lvl, true);
             }
 
             if (side3 === 0) { pntbuf[k] = x3; pntbuf[k+1] = y3; pntbuf[k+2] = z3; k+=3; }
@@ -1582,20 +1579,17 @@
             if (side3!==side1) {
                // order is important, should move from 3->1 point, checked via lastpart
                lastpart = 0;
-               if ((side3<0) || (side1<0)) AddCrossingPoint(x3,y3,z3, x1,y1,z1, lvl-1, 0);
-               if ((side3>0) || (side1>0)) AddCrossingPoint(x3,y3,z3, x1,y1,z1, lvl, 1);
+               if ((side3<0) || (side1<0)) AddCrossingPoint(x3,y3,z3, x1,y1,z1, lvl-1);
+               if ((side3>0) || (side1>0)) AddCrossingPoint(x3,y3,z3, x1,y1,z1, lvl, true);
             }
 
             if (k===0) continue;
             if (k<9) { console.log('FOND less than 3 points', k/3); continue; }
 
-            if (grid) {
-               for (var kk=0;kk<2;++kk)
-                  if (gridcnt[kk] === 6) {
-                    for (var jj=0;jj < 6; ++jj)
-                        grid[gindx+jj] = gridpnts[kk][jj];
-                    gindx+=6;
-                  }
+            if (grid && (gridcnt === 6)) {
+               for (var jj=0;jj < 6; ++jj)
+                  grid[gindx+jj] = gridpnts[jj];
+               gindx+=6;
             }
 
             if (docolorfaces) {
@@ -1630,7 +1624,8 @@
             if (nsegments > 0)
                lpos = new Float32Array(nsegments * 6);
             console.log('numgrids', ngridsegments);
-            grid = new Float32Array(ngridsegments * 6); gindx = 0;
+            if (dogrid && (ngridsegments>0))
+               grid = new Float32Array(ngridsegments * 6);
          }
          for (i=handle.i1;i<handle.i2-1;++i) {
             x1 = handle.grx[i];
