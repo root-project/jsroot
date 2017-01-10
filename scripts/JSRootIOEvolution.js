ea4e6878aaf1e16b644993683511d7f0e067b067
Sergey Linev <S.Linev@gsi.de>
2017-01-10 09:04:03 +0100

    Small optimization

--- scripts/JSRootIOEvolution.js
+++ scripts/JSRootIOEvolution.js
@@ -1848,11 +1848,11 @@
             } else {
                member.nbits = Math.round(element.fXmin);
                if (member.nbits===0) member.nbits = 12;
-               member.dv = new DataView(new ArrayBuffer(16), 0);
+               member.dv = new DataView(new ArrayBuffer(8), 0); // used to cast from uint32 to float32
                member.read = function(buf) {
                   var theExp = buf.ntou1(), theMan = buf.ntou2();
                   this.dv.setUint32(0, (theExp << 23) | ((theMan & ((1<<(this.nbits+1))-1)) << (23-this.nbits)));
-                  return (1<<(this.nbits+1) & theMan) ? -1*this.dv.getFloat32(0) : this.dv.getFloat32(0);
+                  return ((1<<(this.nbits+1) & theMan) ? -1 : 1) * this.dv.getFloat32(0);
                }; 
             }
             
