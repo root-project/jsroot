9105c9851316c0d1ef957b8061eeea082f851ba4
Sergey Linev <S.Linev@gsi.de>
2017-06-26 14:15:46 +0200

    Do not use return value of JSROOT.draw in examples

--- demo/amore.js
+++ demo/amore.js
@@ -1,19 +1,3 @@
-// custom draw function (old code, now not used).
-Amore_Histo_Draw = function(divid, obj, opt) {
-
-   // normal draw function
-   var painter = JSROOT.draw(divid, obj.fVal, opt);
-
-   // redefine update function while it expects histogram as argument
-   // only required if object need to be monitored from http server
-   painter.OldUpdate = painter.UpdateObject;
-   painter.UpdateObject = function(amore) {
-      return this.OldUpdate(amore.fVal);
-   }
-
-   return painter;
-}
-
 // special handling for the amore::core::String_t which is redifinition of TString
 JSROOT.addUserStreamer("amore::core::String_t", "TString");
 
