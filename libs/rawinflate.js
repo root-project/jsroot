a50e3ec7c4b00c488256fb386509d72b4491b989
Sergey Linev <S.Linev@gsi.de>
2018-02-16 11:15:44 +0100

    Integrate LZ4 decompression into rawinflate.js script
    Very compact code, no need to create extra scripts

--- libs/rawinflate.js
+++ libs/rawinflate.js
@@ -729,6 +729,77 @@ JSROOT.ZIP.inflate = function(arr, tgt)
    return cnt;
 }
 
+/**
+ * https://github.com/pierrec/node-lz4/blob/master/lib/binding.js
+ *
+ * LZ4 based compression and decompression
+ * Copyright (c) 2014 Pierre Curto
+ * MIT Licensed
+ */
+
+JSROOT.LZ4 = {};
+/**
+ * Decode a block. Assumptions: input contains all sequences of a
+ * chunk, output is large enough to receive the decoded data.
+ * If the output buffer is too small, an error will be thrown.
+ * If the returned value is negative, an error occured at the returned offset.
+ *
+ * @param input {Buffer} input data
+ * @param output {Buffer} output data
+ * @return {Number} number of decoded bytes
+ * @private
+ */
+JSROOT.LZ4.uncompress = function (input, output, sIdx, eIdx) {
+   sIdx = sIdx || 0
+   eIdx = eIdx || (input.length - sIdx)
+   // Process each sequence in the incoming data
+   for (var i = sIdx, n = eIdx, j = 0; i < n;) {
+      var token = input[i++]
+
+      // Literals
+      var literals_length = (token >> 4)
+      if (literals_length > 0) {
+         // length of literals
+         var l = literals_length + 240
+         while (l === 255) {
+            l = input[i++]
+            literals_length += l
+         }
+
+         // Copy the literals
+         var end = i + literals_length
+         while (i < end) output[j++] = input[i++]
+
+         // End of buffer?
+         if (i === n) return j
+      }
+
+      // Match copy
+      // 2 bytes offset (little endian)
+      var offset = input[i++] | (input[i++] << 8)
+
+      // 0 is an invalid offset value
+      if (offset === 0 || offset > j) return -(i-2)
+
+      // length of match copy
+      var match_length = (token & 0xf)
+      var l = match_length + 240
+      while (l === 255) {
+         l = input[i++]
+         match_length += l
+      }
+
+      // Copy the match
+      var pos = j - offset // position of the match copy in the current output
+      var end = j + match_length + 4 // minmatch = 4
+      while (j < end) output[j++] = output[pos++]
+   }
+
+   return j
+}
+
+
+
 return JSROOT;
 
 }));
