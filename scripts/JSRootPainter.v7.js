c7f81af8378edd5609585fc166bf724c64f92679
Olivier Couet <olivier.couet@cern.ch>
2018-03-12 17:08:21 +0100

    add drawline  function in v7 graphics

--- scripts/JSRootPainter.v7.js
+++ scripts/JSRootPainter.v7.js
@@ -4062,6 +4062,7 @@
    JSROOT.addDrawFunc({ name: "ROOT::Experimental::THistDrawable<1>", icon: "img_histo1d", prereq: "v7hist", func: "JSROOT.v7.drawHist1", opt: "" });
    JSROOT.addDrawFunc({ name: "ROOT::Experimental::THistDrawable<2>", icon: "img_histo2d", prereq: "v7hist", func: "JSROOT.v7.drawHist2", opt: "" });
    JSROOT.addDrawFunc({ name: "ROOT::Experimental::TText", icon: "img_text", prereq: "v7more", func: "JSROOT.v7.drawText", opt: "", direct: true });
+   JSROOT.addDrawFunc({ name: "ROOT::Experimental::TLine", icon: "img_graph", prereq: "v7more", func: "JSROOT.v7.drawLine", opt: "", direct: true });
 
 
    JSROOT.v7.TAxisPainter = TAxisPainter;
