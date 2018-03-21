b8c629c11cf53b7ebf86d74b79860b27c29a9acd
Sergey Linev <S.Linev@gsi.de>
2018-03-21 14:27:42 +0100

    Fix - when set pad logx/logy, reset user ranges
    If changing log setting, one need to recalculate user ranges.
    To avoid not trivial calculations, just reset user ranges - they will be
    recalculated automatically by histogram painter

--- scripts/JSRootPainter.v6.js
+++ scripts/JSRootPainter.v6.js
@@ -4048,8 +4048,8 @@
       if (d.check("CP",true)) this.options.CreatePalette = d.partAsInt(0,0);
 
       if (d.check('WHITE')) pad.fFillColor = 0;
-      if (d.check('LOGX')) pad.fLogx = 1;
-      if (d.check('LOGY')) pad.fLogy = 1;
+      if (d.check('LOGX')) { pad.fLogx = 1; pad.fUxmin = 0; pad.fUxmax = 1; pad.fX1 = 0; pad.fX2 = 1; }
+      if (d.check('LOGY')) { pad.fLogy = 1; pad.fUymin = 0; pad.fUymax = 1; pad.fY1 = 0; pad.fY2 = 1; }
       if (d.check('LOGZ')) pad.fLogz = 1;
       if (d.check('LOG')) pad.fLogx = pad.fLogy = pad.fLogz = 1;
       if (d.check('GRIDX')) pad.fGridx = 1;
