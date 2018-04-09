c0c8e56bc9425a49111fbb51a0f3aaa20d11e52c
Sergey Linev <S.Linev@gsi.de>
2018-04-09 13:22:57 +0200

    When too many graphs - avoid deep calling stack (#154)

--- scripts/JSRootPainter.more.js
+++ scripts/JSRootPainter.more.js
@@ -3160,7 +3160,7 @@
          histo.fXaxis.fXmax = uxmax;
 
      }
- 
+
       histo.fYaxis.fXmin = minimum;
       histo.fYaxis.fXmax = maximum;
 
@@ -3190,7 +3190,7 @@
                   this.DrawNextFunction.bind(this, indx+1, callback));
    }
 
-   TMultiGraphPainter.prototype.DrawNextGraph = function(indx, opt, subp) {
+   TMultiGraphPainter.prototype.DrawNextGraph = function(indx, opt, subp, used_timeout) {
       if (subp) this.painters.push(subp);
 
       var graphs = this.GetObject().fGraphs;
@@ -3200,6 +3200,10 @@
          this._pfc = this._plc = this._pmc = false; // disable auto coloring at the end
          return this.DrawNextFunction(0, this.DrawingReady.bind(this));
       }
+
+      // when too many graphs are drawn, avoid deep stack with timeout
+      if ((indx % 500 === 499) && !used_timeout)
+         return setTimeout(this.DrawNextGraph.bind(this,indx,opt,null,true),0);
 
       // if there is auto colors assignment, try to provide it
       if (this._pfc || this._plc || this._pmc) {
