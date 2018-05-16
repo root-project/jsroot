64867aee75f8f7dff1c090ff4c66bfb74265e223
Sergey Linev <S.Linev@gsi.de>
2018-05-16 12:50:27 +0200

    Fix - return only TF1/TF2 object when searching function (#158)

--- scripts/JSRootPainter.more.js
+++ scripts/JSRootPainter.more.js
@@ -1809,7 +1809,7 @@
             var func = gr.fFunctions.arr[i];
             if ((func._typename == 'TF1') || (func._typename == 'TF2')) return func;
          }
-      return func;
+      return null;
    }
 
    TGraphPainter.prototype.FindStat = function() {
