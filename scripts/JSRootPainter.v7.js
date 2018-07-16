cad5d499b6fed791de835c53a218e729593cf7e1
Sergey Linev <S.Linev@gsi.de>
2018-07-16 12:50:52 +0200

    Fix - correct names of v7 classes

--- scripts/JSRootPainter.v7.js
+++ scripts/JSRootPainter.v7.js
@@ -2865,7 +2865,7 @@
 
       if (arr)
          for (var n=0;n<arr.length;++n)
-            if (arr[n] && arr[n]._typename != "ROOT::Experimental::TPadDisplayItem") return true;
+            if (arr[n] && arr[n]._typename != "ROOT::Experimental::RPadDisplayItem") return true;
 
       return false;
    }
@@ -3154,7 +3154,7 @@
 
          if (objpainter) {
 
-            if (snap._typename == "ROOT::Experimental::TPadDisplayItem")  // subpad
+            if (snap._typename == "ROOT::Experimental::RPadDisplayItem")  // subpad
                return objpainter.RedrawPadSnap(snap, draw_callback);
 
             if (objpainter.UpdateObject(snap.fObject, snap.fOption || "")) objpainter.Redraw();
@@ -3162,7 +3162,7 @@
             continue; // call next
          }
 
-         if (snap._typename == "ROOT::Experimental::TPadDisplayItem") { // subpad
+         if (snap._typename == "ROOT::Experimental::RPadDisplayItem") { // subpad
 
             var subpad = snap; // not subpad, but just attributes
 
@@ -4110,10 +4110,10 @@
       return painter;
    }
 
-   // JSROOT.addDrawFunc({ name: "ROOT::Experimental::TPadDisplayItem", icon: "img_canvas", func: drawPad, opt: "" });
+   // JSROOT.addDrawFunc({ name: "ROOT::Experimental::RPadDisplayItem", icon: "img_canvas", func: drawPad, opt: "" });
 
-   JSROOT.addDrawFunc({ name: "ROOT::Experimental::THistDrawable<1>", icon: "img_histo1d", prereq: "v7hist", func: "JSROOT.v7.drawHist1", opt: "" });
-   JSROOT.addDrawFunc({ name: "ROOT::Experimental::THistDrawable<2>", icon: "img_histo2d", prereq: "v7hist", func: "JSROOT.v7.drawHist2", opt: "" });
+   JSROOT.addDrawFunc({ name: "ROOT::Experimental::RHistDrawable<1>", icon: "img_histo1d", prereq: "v7hist", func: "JSROOT.v7.drawHist1", opt: "" });
+   JSROOT.addDrawFunc({ name: "ROOT::Experimental::RHistDrawable<2>", icon: "img_histo2d", prereq: "v7hist", func: "JSROOT.v7.drawHist2", opt: "" });
    JSROOT.addDrawFunc({ name: "ROOT::Experimental::RText", icon: "img_text", prereq: "v7more", func: "JSROOT.v7.drawText", opt: "", direct: true });
    JSROOT.addDrawFunc({ name: "ROOT::Experimental::RLine", icon: "img_graph", prereq: "v7more", func: "JSROOT.v7.drawLine", opt: "", direct: true });
    JSROOT.addDrawFunc({ name: "ROOT::Experimental::RMarker", icon: "img_graph", prereq: "v7more", func: "JSROOT.v7.drawMarker", opt: "", direct: true });
