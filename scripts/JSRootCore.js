28ae458e5ef94cd973b74d50e8b54be90197f752
PEERCRED <45268116+PEERCRED@users.noreply.github.com>
2018-11-26 22:39:38 +0300

    Fix 'blob:' URL handling in Chromium

--- scripts/JSRootCore.js
+++ scripts/JSRootCore.js
@@ -882,7 +882,7 @@
          if (xhr.readyState != 4) return;
 
          if ((xhr.status != 200) && (xhr.status != 206) && !JSROOT.browser.qt5 &&
-               ((xhr.status !== 0) || (url.indexOf("file://")!==0))) {
+               ((xhr.status !== 0) || (url.indexOf("file://")!==0) && (url.indexOf("blob:")!==0))) {
             return callback(null);
          }
 
