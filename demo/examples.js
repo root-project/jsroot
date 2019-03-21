ba2e3084fee70e16092f822fc3b32aeb90f50042
Sergey Linev <S.Linev@gsi.de>
2019-03-21 13:27:34 +0100

    Add TH2 projections to examples

--- demo/examples.js
+++ demo/examples.js
@@ -28,6 +28,8 @@ examples_main = {
      { opt: "colpal77", title: "example how color palette 77 can be used for drawing" },
      { opt: "colz", title: "color draw with palette" },
      { opt: "acol", title: "disable axes drawing with col option" },
+     { name: "projx1", opt: "projx1,colz", title: "Draw X projection together with 2D plot", notest:true },
+     { name: "projy3", opt: "projy3,colz", title: "Draw Y projection over 3 bins together with 2D plot", notest:true },
      { opt: "arr", title: "ARR draw option" },
      { opt: "cont", title: "Contour plot" },
      { opt: "cont1", title: "Contour plot with different line colors" },
