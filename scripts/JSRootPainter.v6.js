54811ef8ce1beedc6c096693bf0421b377201cfe
Sergey Linev <S.Linev@gsi.de>
2019-04-16 13:52:25 +0200

    Fix - when doing projection, "hide" mdi layout, fixes resize
    Layout only used during construction.
    If object reference preserved in HTML element, resize handler redirect
    call to MDI which does not work.

--- scripts/JSRootPainter.v6.js
+++ scripts/JSRootPainter.v6.js
@@ -4453,6 +4453,9 @@
 
          pthis.set_layout_kind(layout_kind, ".central_panel");
 
+         // remove reference to MDIDisplay, solves resize problem
+         origin.property('mdi', null);
+
          JSROOT.CallBack(call_back, true);
       });
    }
