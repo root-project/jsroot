8fa853c5c0eb6b229bf2ebedb689bccc2f1860a2
Sergey Linev <S.Linev@gsi.de>
2020-11-25 10:32:13 +0100

    Use promises when reading TTree data

--- scripts/JSRoot.tree.js
+++ scripts/JSRoot.tree.js
@@ -2074,15 +2074,13 @@ JSROOT.define(['io', 'math'], (jsrio, jsrmath) => {
          handle.process_arrays = false;
       }
 
-      function ReadBaskets(bitems, baskets_call_back) {
-         // read basket with tree data, selecting different files
-
-         let places = [], filename = "";
+      /** read basket with tree data, selecting different files */
+      function ReadBaskets(bitems) {
 
          function ExtractPlaces() {
             // extract places to read and define file name
 
-            places = []; filename = "";
+            let places = [], filename = "";
 
             for (let n = 0; n < bitems.length; ++n) {
                if (bitems[n].done) continue;
@@ -2091,15 +2089,15 @@ JSROOT.define(['io', 'math'], (jsrio, jsrmath) => {
 
                if (places.length === 0)
                   filename = branch.fFileName;
-               else
-                  if (filename !== branch.fFileName) continue;
+               else if (filename !== branch.fFileName)
+                  continue;
 
                bitems[n].selected = true; // mark which item was selected for reading
 
                places.push(branch.fBasketSeek[bitems[n].basket], branch.fBasketBytes[bitems[n].basket]);
             }
 
-            return places.length > 0;
+            return places.length > 0 ? { places: places, filename: filename } : null;
          }
 
          function ReadProgress(value) {
@@ -2119,13 +2117,13 @@ JSROOT.define(['io', 'math'], (jsrio, jsrmath) => {
             handle.selector.ShowProgress(portion);
          }
 
-         function ProcessBlobs(blobs) {
+         function ProcessBlobs(blobs, places) {
             if (!blobs || ((places.length > 2) && (blobs.length * 2 !== places.length)))
-               return JSROOT.callBack(baskets_call_back, null);
+               return Promise.resolve(null);
 
-            let n = 0, k = 0;
+            if (places.length == 2) blobs = [ blobs ];
 
-            function DoProcessing() {
+            function DoProcessing(k) {
 
                for (; k < bitems.length; ++k) {
                   if (!bitems[k].selected) continue;
@@ -2133,7 +2131,7 @@ JSROOT.define(['io', 'math'], (jsrio, jsrmath) => {
                   bitems[k].selected = false;
                   bitems[k].done = true;
 
-                  let blob = (places.length > 2) ? blobs[n++] : blobs,
+                  let blob = blobs.shift(),
                      buf = new JSROOT.TBuffer(blob, 0, handle.file),
                      basket = buf.ClassStreamer({}, "TBasket");
 
@@ -2172,24 +2170,28 @@ JSROOT.define(['io', 'math'], (jsrio, jsrmath) => {
                      if (bitems[k].branch.fEntryOffsetLen > 0)
                         buf.ReadBasketEntryOffset(basket, buf.raw_shift);
 
-                     DoProcessing();  // continue processing
+                     return DoProcessing(k+1);  // continue processing
                   });
                }
 
-               if (ExtractPlaces())
-                  handle.file.ReadBuffer(places, filename, ReadProgress).then(ProcessBlobs).catch(() => ProcessBlobs(null));
-               else
-                  JSROOT.callBack(baskets_call_back, bitems);
+               let req = ExtractPlaces();
+
+               if (req)
+                  return handle.file.ReadBuffer(req.places, req.filename, ReadProgress).then(blobs => ProcessBlobs(blobs)).catch(() => { return null; });
+
+               return Promise.resolve(bitems);
              }
 
-             DoProcessing();
+             return DoProcessing(0);
          }
 
+         let req = ExtractPlaces();
+
          // extract places where to read
-         if (ExtractPlaces())
-            handle.file.ReadBuffer(places, filename, ReadProgress).then(ProcessBlobs).catch(() => ProcessBlobs(null));
-         else
-            JSROOT.callBack(baskets_call_back, null);
+         if (req)
+            return handle.file.ReadBuffer(req.places, req.filename, ReadProgress).then(blobs => ProcessBlobs(blobs, req.places)).catch(() => { return null; });
+
+         return Promise.resolve(null);
       }
 
       function ReadNextBaskets() {
@@ -2282,7 +2284,7 @@ JSROOT.define(['io', 'math'], (jsrio, jsrmath) => {
 
          handle.progress_showtm = new Date().getTime();
 
-         if (totalsz > 0) return ReadBaskets(bitems, ProcessBaskets);
+         if (totalsz > 0) return ReadBaskets(bitems).then(ProcessBaskets);
 
          if (is_direct) return ProcessBaskets([]); // directly process baskets
 
@@ -2867,8 +2869,8 @@ JSROOT.define(['io', 'math'], (jsrio, jsrmath) => {
          if (create_player === 1) { last_intermediate = intermediate; return; }
 
          // redirect drawing to the player
-         player_create = 1;
-         args.player_intermediate = res.progress;
+         create_player = 1;
+         // args.player_intermediate = res.progress;
          JSROOT.require("jq2d").then(() => {
             JSROOT.CreateTreePlayer(painter);
             painter.ConfigureTree(tree);
