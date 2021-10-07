5972e8610c3f05e15be24bda06ba909cd19fc6f1
Sergey Linev <S.Linev@gsi.de>
2021-10-07 15:50:56 +0200

    Fix error in projection statistic calculations

--- scripts/JSRoot.hist.js
+++ scripts/JSRoot.hist.js
@@ -4698,6 +4698,10 @@ JSROOT.define(['d3', 'painter', 'gpad'], (d3, jsrp) => {
          }
       }
 
+      // reset statistic before display
+      this.proj_hist.fEntries = 0;
+      this.proj_hist.fTsumw = 0;
+
       return canp.drawProjection(this.is_projection, this.proj_hist);
    }
 
