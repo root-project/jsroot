f7605b9f068e59182bad3cf3d9c18fe4fe6e239c
Sergey Linev <S.Linev@gsi.de>
2021-11-29 11:08:29 +0100

    Remove old files for building three.extra.js - no longer necessary
    Now everything packed into custom three.js, which is also used in
    node.js

--- libs/three.svg_renderer_header.js
+++
@@ -1,8 +0,0 @@
-// wrapper for THREE.SVGRenderer trying to optimize creation of many small SVG elements
-
-THREE.CreateSVGRenderer = function(as_is, precision, doc) {
-
-   function Create(document) {
-
-// now include original THREE.SVGRenderer
-//
