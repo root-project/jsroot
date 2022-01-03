a2220f8257313260859a685ffd02920e3651a159
Sergey Linev <S.Linev@gsi.de>
2022-01-03 11:42:18 +0100

    Introduce erf/efrc in math script

--- scripts/JSRoot.math.js
+++ scripts/JSRoot.math.js
@@ -25,7 +25,7 @@ JSROOT.define([], () =>  {
       if (N==0) return a[0];
 
       let pom = a[0];
-      for (let i=1; i <= N; ++i)
+      for (let i = 1; i <= N; ++i)
          pom = pom *x + a[i];
       return pom;
    }
@@ -38,7 +38,7 @@ JSROOT.define([], () =>  {
       if (N==0) return a[0];
 
       let pom = x + a[0];
-      for (let i=1; i < N; ++i)
+      for (let i = 1; i < N; ++i)
          pom = pom *x + a[i];
       return pom;
    }
@@ -164,6 +164,105 @@ JSROOT.define([], () =>  {
          y = Math.pow( x, x - 0.5 ) / y;
       }
       y = SQTPI * y * w;
+      return y;
+   }
+
+
+   /** @summary complementary error function
+     * @private */
+   function erfc(a) {
+      const erfP = [
+         2.46196981473530512524E-10,
+         5.64189564831068821977E-1,
+         7.46321056442269912687E0,
+         4.86371970985681366614E1,
+         1.96520832956077098242E2,
+         5.26445194995477358631E2,
+         9.34528527171957607540E2,
+         1.02755188689515710272E3,
+         5.57535335369399327526E2
+      ], erfQ = [
+         1.32281951154744992508E1,
+         8.67072140885989742329E1,
+         3.54937778887819891062E2,
+         9.75708501743205489753E2,
+         1.82390916687909736289E3,
+         2.24633760818710981792E3,
+         1.65666309194161350182E3,
+         5.57535340817727675546E2
+      ], erfR = [
+         5.64189583547755073984E-1,
+         1.27536670759978104416E0,
+         5.01905042251180477414E0,
+         6.16021097993053585195E0,
+         7.40974269950448939160E0,
+         2.97886665372100240670E0
+      ], erfS = [
+         2.26052863220117276590E0,
+         9.39603524938001434673E0,
+         1.20489539808096656605E1,
+         1.70814450747565897222E1,
+         9.60896809063285878198E0,
+         3.36907645100081516050E0
+      ];
+
+      let p,q,x,y,z;
+
+      if( a < 0.0 )
+         x = -a;
+      else
+         x = a;
+
+      if( x < 1.0 )
+         return 1.0 - mth.erf(a);
+
+      z = -a * a;
+
+      if(z < -kMAXLOG)
+         return (a < 0) ? 2.0 : 0.0;
+
+      z = Math.exp(z);
+
+      if( x < 8.0 ) {
+         p = Polynomialeval( x, erfP, 8 );
+         q = Polynomial1eval( x, erfQ, 8 );
+      } else {
+         p = Polynomialeval( x, erfR, 5 );
+         q = Polynomial1eval( x, erfS, 6 );
+      }
+      y = (z * p)/q;
+
+      if(a < 0)
+         y = 2.0 - y;
+
+      if(y == 0)
+         return (a < 0) ? 2.0 : 0.0;
+
+      return y;
+   }
+
+   /** @summary error function
+     * @private */
+   function erf(x) {
+      if(Math.abs(x) > 1.0)
+         return 1.0 - erfc(x);
+
+      const erfT = [
+         9.60497373987051638749E0,
+         9.00260197203842689217E1,
+         2.23200534594684319226E3,
+         7.00332514112805075473E3,
+         5.55923013010394962768E4
+      ], erfU = [
+         3.35617141647503099647E1,
+         5.21357949780152679795E2,
+         4.59432382970980127987E3,
+         2.26290000613890934246E4,
+         4.92673942608635921086E4
+      ];
+
+      let z = x * x;
+      let y = x * Polynomialeval( z, erfT, 4 ) / Polynomial1eval( z, erfU, 5 );
       return y;
    }
 
@@ -1583,6 +1682,8 @@ JSROOT.define([], () =>  {
    mth.igami = igami;
    mth.igamc = igamc;
    mth.igam = igam;
+   mth.erfc = erfc;
+   mth.erf = erf;
    mth.beta_pdf = beta_pdf;
    mth.inc_beta = inc_beta;
    mth.pseries = pseries;
