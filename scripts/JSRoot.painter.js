d17ead5eab77b431866e8836eef69da845b6ef97
Sergey Linev <S.Linev@gsi.de>
2022-01-04 12:10:52 +0100

    Convert TAttLineHandler into class

--- scripts/JSRoot.painter.js
+++ scripts/JSRoot.painter.js
@@ -427,7 +427,7 @@ JSROOT.define(['d3'], (d3) => {
    class TAttMarkerHandler {
 
       /** @summary constructor
-        * @param {object} args - attributes, see {@link JSROOT.TAttMarkerHandler.setArgs} for details */
+        * @param {object} args - attributes, see {@link JSROOT.TAttMarkerHandler#setArgs} for details */
       constructor(args) {
          this.x0 = this.y0 = 0;
          this.color = 'black';
@@ -701,127 +701,130 @@ JSROOT.define(['d3'], (d3) => {
      *
      * @class
      * @memberof JSROOT
-     * @param {object} attr - TAttLine object
      * @private
      */
 
-   function TAttLineHandler(args) {
-      this.func = this.apply.bind(this);
-      this.used = true;
-      if (args._typename && (args.fLineStyle !== undefined)) args = { attr: args };
-
-      this.setArgs(args);
-   }
-
-   /** @summary Set line attributes.
-     * @param {object} args - specify attributes by different ways
-     * @param {object} args.attr - TAttLine object with appropriate data members or
-     * @param {string} args.color - color in html like rgb(255,0,0) or "red" or "#ff0000"
-     * @param {number} args.style - line style number
-     * @param {number} args.width - line width */
-   TAttLineHandler.prototype.setArgs = function(args) {
-      if (args.attr) {
-         args.color = args.color0 || (args.painter ? args.painter.getColor(args.attr.fLineColor) : jsrp.getColor(args.attr.fLineColor));
-         if (args.width === undefined) args.width = args.attr.fLineWidth;
-         if (args.style === undefined) args.style = args.attr.fLineStyle;
-      } else if (typeof args.color == 'string') {
-         if ((args.color !== 'none') && !args.width) args.width = 1;
-      } else if (typeof args.color == 'number') {
-         args.color = args.painter ? args.painter.getColor(args.color) : jsrp.getColor(args.color);
+   class TAttLineHandler {
+      
+      /** @summary constructor
+        * @param {object} attr - attributes, see {@link JSROOT.TAttLineHandler#setArgs} */
+      constructor(args) {
+         this.func = this.apply.bind(this);
+         this.used = true;
+         if (args._typename && (args.fLineStyle !== undefined)) args = { attr: args };
+         this.setArgs(args);
       }
-
-      if (args.width === undefined)
-         args.width = (args.color && args.color != 'none') ? 1 : 0;
-
-      this.color = (args.width === 0) ? 'none' : args.color;
-      this.width = args.width;
-      this.style = args.style;
-      this.pattern = args.pattern || jsrp.root_line_styles[this.style] || null;
-
-      if (args.can_excl) {
-         this.excl_side = this.excl_width = 0;
-         if (Math.abs(this.width) > 99) {
-            // exclusion graph
-            this.excl_side = (this.width < 0) ? -1 : 1;
-            this.excl_width = Math.floor(this.width / 100) * 5;
-            this.width = Math.abs(this.width % 100); // line width
+   
+      /** @summary Set line attributes.
+        * @param {object} args - specify attributes by different ways
+        * @param {object} args.attr - TAttLine object with appropriate data members or
+        * @param {string} args.color - color in html like rgb(255,0,0) or "red" or "#ff0000"
+        * @param {number} args.style - line style number
+        * @param {number} args.width - line width */
+      setArgs(args) {
+         if (args.attr) {
+            args.color = args.color0 || (args.painter ? args.painter.getColor(args.attr.fLineColor) : jsrp.getColor(args.attr.fLineColor));
+            if (args.width === undefined) args.width = args.attr.fLineWidth;
+            if (args.style === undefined) args.style = args.attr.fLineStyle;
+         } else if (typeof args.color == 'string') {
+            if ((args.color !== 'none') && !args.width) args.width = 1;
+         } else if (typeof args.color == 'number') {
+            args.color = args.painter ? args.painter.getColor(args.color) : jsrp.getColor(args.color);
          }
+   
+         if (args.width === undefined)
+            args.width = (args.color && args.color != 'none') ? 1 : 0;
+   
+         this.color = (args.width === 0) ? 'none' : args.color;
+         this.width = args.width;
+         this.style = args.style;
+         this.pattern = args.pattern || jsrp.root_line_styles[this.style] || null;
+   
+         if (args.can_excl) {
+            this.excl_side = this.excl_width = 0;
+            if (Math.abs(this.width) > 99) {
+               // exclusion graph
+               this.excl_side = (this.width < 0) ? -1 : 1;
+               this.excl_width = Math.floor(this.width / 100) * 5;
+               this.width = Math.abs(this.width % 100); // line width
+            }
+         }
+   
+         // if custom color number used, use lightgrey color to show lines
+         if (!this.color && (this.width > 0))
+            this.color = 'lightgrey';
       }
-
-      // if custom color number used, use lightgrey color to show lines
-      if (!this.color && (this.width > 0))
-         this.color = 'lightgrey';
-   }
-
-   /** @summary Change exclusion attributes */
-   TAttLineHandler.prototype.changeExcl = function(side, width) {
-      if (width !== undefined)
-         this.excl_width = width;
-      if (side !== undefined) {
-         this.excl_side = side;
-         if ((this.excl_width === 0) && (this.excl_side !== 0)) this.excl_width = 20;
+   
+      /** @summary Change exclusion attributes */
+      changeExcl(side, width) {
+         if (width !== undefined)
+            this.excl_width = width;
+         if (side !== undefined) {
+            this.excl_side = side;
+            if ((this.excl_width === 0) && (this.excl_side !== 0)) this.excl_width = 20;
+         }
+         this.changed = true;
       }
-      this.changed = true;
-   }
-
-   /** @summary returns true if line attribute is empty and will not be applied. */
-   TAttLineHandler.prototype.empty = function() { return this.color == 'none'; }
-
-   /** @summary set border parameters, used for rect drawing */
-   TAttLineHandler.prototype.setBorder = function(rx, ry) {
-      this.rx = rx;
-      this.ry = ry;
-      this.func = this.applyBorder.bind(this);
-   }
-
-   /** @summary Applies line attribute to selection.
-     * @param {object} selection - d3.js selection */
-   TAttLineHandler.prototype.apply = function(selection) {
-      this.used = true;
-      if (this.empty())
-         selection.style('stroke', null)
-                  .style('stroke-width', null)
-                  .style('stroke-dasharray', null);
-      else
-         selection.style('stroke', this.color)
-                  .style('stroke-width', this.width)
-                  .style('stroke-dasharray', this.pattern);
-   }
-
-   /** @summary Applies line and border attribute to selection.
-     * @param {object} selection - d3.js selection */
-   TAttLineHandler.prototype.applyBorder = function(selection) {
-      this.used = true;
-      if (this.empty())
-         selection.style('stroke', null)
-                  .style('stroke-width', null)
-                  .style('stroke-dasharray', null)
-                  .attr("rx", null).attr("ry", null);
-      else
-         selection.style('stroke', this.color)
-                  .style('stroke-width', this.width)
-                  .style('stroke-dasharray', this.pattern)
-                  .attr("rx", this.rx || null).attr("ry", this.ry || null);
-   }
-
-   /** @summary Change line attributes */
-   TAttLineHandler.prototype.change = function(color, width, style) {
-      if (color !== undefined) this.color = color;
-      if (width !== undefined) this.width = width;
-      if (style !== undefined) {
-         this.style = style;
-         this.pattern = jsrp.root_line_styles[this.style] || null;
+   
+      /** @summary returns true if line attribute is empty and will not be applied. */
+      empty() { return this.color == 'none'; }
+   
+      /** @summary set border parameters, used for rect drawing */
+      setBorder(rx, ry) {
+         this.rx = rx;
+         this.ry = ry;
+         this.func = this.applyBorder.bind(this);
       }
-      this.changed = true;
+   
+      /** @summary Applies line attribute to selection.
+        * @param {object} selection - d3.js selection */
+      apply(selection) {
+         this.used = true;
+         if (this.empty())
+            selection.style('stroke', null)
+                     .style('stroke-width', null)
+                     .style('stroke-dasharray', null);
+         else
+            selection.style('stroke', this.color)
+                     .style('stroke-width', this.width)
+                     .style('stroke-dasharray', this.pattern);
+      }
+   
+      /** @summary Applies line and border attribute to selection.
+        * @param {object} selection - d3.js selection */
+      applyBorder(selection) {
+         this.used = true;
+         if (this.empty())
+            selection.style('stroke', null)
+                     .style('stroke-width', null)
+                     .style('stroke-dasharray', null)
+                     .attr("rx", null).attr("ry", null);
+         else
+            selection.style('stroke', this.color)
+                     .style('stroke-width', this.width)
+                     .style('stroke-dasharray', this.pattern)
+                     .attr("rx", this.rx || null).attr("ry", this.ry || null);
+      }
+   
+      /** @summary Change line attributes */
+      change(color, width, style) {
+         if (color !== undefined) this.color = color;
+         if (width !== undefined) this.width = width;
+         if (style !== undefined) {
+            this.style = style;
+            this.pattern = jsrp.root_line_styles[this.style] || null;
+         }
+         this.changed = true;
+      }
+   
+      /** @summary Create sample element inside primitive SVG - used in context menu */
+      createSample(svg, width, height) {
+         svg.append("path")
+            .attr("d", `M0,${height/2}h${width}`)
+            .call(this.func);
+      }
    }
-
-   /** @summary Create sample element inside primitive SVG - used in context menu */
-   TAttLineHandler.prototype.createSample = function(svg, width, height) {
-      svg.append("path")
-         .attr("d", `M0,${height/2}h${width}`)
-         .call(this.func);
-   }
-
+ 
    // =======================================================================
 
    /**
