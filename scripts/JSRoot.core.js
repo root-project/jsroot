d0cb55e362042de9812859cb77a3c772cc9d5909
Sergey Linev <S.Linev@gsi.de>
2022-02-23 14:58:15 +0100

    Adjust examples.htm

--- scripts/JSRoot.core.js
+++ scripts/JSRoot.core.js
@@ -31,8 +31,6 @@ let nodejs = (export_kind === 'node');
   * @alias JSROOT.batch_mode */
 exports.batch_mode = nodejs;
 
-//openuicfg // DO NOT DELETE, used to configure openui5 usage like JSROOT.openui5src = "nojsroot";
-
 /** @summary internal data
   * @memberof JSROOT
   * @private */
@@ -1871,5 +1869,7 @@ exports.connectWebWindow = connectWebWindow;
 // try to define global JSROOT
 if (typeof globalThis !== "undefined")
    globalThis.JSROOT = exports;
+
+//openuicfg // DO NOT DELETE, used to configure openui5 usage like JSROOT.openui5src = "nojsroot";
 
 }));
