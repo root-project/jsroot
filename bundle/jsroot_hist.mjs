d5f1fc9c6dde54de330cf53345dc5bf11e624de5
Sergey Linev <S.Linev@gsi.de>
2022-03-10 08:53:58 +0100

    Provide usage example for bundled package

--- bundle/jsroot_hist.mjs
+++ bundle/jsroot_hist.mjs
@@ -1,3 +1,3 @@
-export { version, cleanup } from "../modules/core.mjs";
+export { version, cleanup, parse } from "../modules/core.mjs";
 
 export { TH1Painter, TH2Painter } from "../modules/hist.mjs";
