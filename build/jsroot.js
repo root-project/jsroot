86a994b170c0433d11e92787da5b26e1d78e8bf6
Sergey Linev <S.Linev@gsi.de>
2022-06-07 15:56:23 +0200

    build

--- build/jsroot.js
+++ build/jsroot.js
@@ -3652,6 +3652,30 @@ function select(selector) {
       : new Selection$1([[selector]], root$1);
 }
 
+var nextId = 0;
+
+function Local() {
+  this._ = "@" + (++nextId).toString(36);
+}
+
+Local.prototype = {
+  constructor: Local,
+  get: function(node) {
+    var id = this._;
+    while (!(id in node)) if (!(node = node.parentNode)) return;
+    return node[id];
+  },
+  set: function(node, value) {
+    return node[this._] = value;
+  },
+  remove: function(node) {
+    return this._ in node && delete node[this._];
+  },
+  toString: function() {
+    return this._;
+  }
+};
+
 function sourceEvent(event) {
   let sourceEvent;
   while (sourceEvent = event.sourceEvent) event = sourceEvent;
@@ -45497,7 +45521,7 @@ class TAxisPainter extends ObjectPainter {
             }
 
             // for order==0 we should virtually remove "0." and extra label on top
-            if (!order && (this.ndig<4)) totallen-=(handle.major.length*2+3);
+            if (!order && (this.ndig < 4)) totallen -= (handle.major.length*2+3);
 
             if (totallen < bestlen) {
                bestlen = totallen;
@@ -45591,8 +45615,8 @@ class TAxisPainter extends ObjectPainter {
                acc_x += evnt.dx;
                acc_y += evnt.dy;
 
-               let set_x, set_y,
-                   p = vertical ? acc_y : acc_x, besti = 0;
+               let set_x, set_y, besti = 0,
+                   p = vertical ? acc_y : acc_x;
 
                for (let i=1; i<3; ++i)
                   if (Math.abs(p - alt_pos[i]) < Math.abs(p - alt_pos[besti])) besti = i;
@@ -45607,7 +45631,7 @@ class TAxisPainter extends ObjectPainter {
 
                if (sign_0 === (vertical ? (set_x > 0) : (set_y > 0))) {
                   new_x = set_x; new_y = set_y; curr_indx = besti;
-                  title_g.attr('transform', 'translate(' + new_x + ',' + new_y +  ')');
+                  title_g.attr('transform', `translate(${new_x},${new_y})`);
                }
 
           }).on("end", evnt => {
