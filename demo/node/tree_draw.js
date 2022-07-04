6c0be0d3f7897c552680f91ea7e8758cc2b0e584
Sergey Linev <S.Linev@gsi.de>
2022-07-04 14:48:13 +0200

    Fix tree_draw.js example

--- demo/node/tree_draw.js
+++ demo/node/tree_draw.js
@@ -1,4 +1,4 @@
-import { version, httpRequest, openFile, makeSVG } from "jsroot";
+import { version, httpRequest, openFile, makeSVG, treeDraw } from "jsroot";
 
 import { writeFileSync } from "fs";
 
@@ -14,7 +14,7 @@ let file = await openFile(arg);
 
 // now read ntuple, perform Draw operation, create SVG file and sve to the disk
 let ntuple = await file.readObject("ntuple");
-let hist = await ntuple.Draw("px:py::pz>5");
+let hist = await treeDraw(ntuple, "px:py::pz>5");
 let svg = await makeSVG({ object: hist, width: 1200, height: 800 });
 writeFileSync("tree_draw.svg", svg);
 console.log(`Create tree_draw.svg size ${svg.length}`);
