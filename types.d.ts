60c22ed49e269b8e435acd30ec8c213dfabf051b
Fawad Ali <m.fawaadali98@gmail.com>
2022-09-26 12:47:43 +0500

    Add basic TypeScript types (#246)

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+declare module "jsroot";
+declare module "jsroot/core";
+declare module "jsroot/draw";
+declare module "jsroot/io";
+declare module "jsroot/tree";
+declare module "jsroot/colors";
+declare module "jsroot/hierarchy";
+declare module "jsroot/latex";
+declare module "jsroot/geom";
