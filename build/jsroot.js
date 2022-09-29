9e1a9bd08e283b0aafa3fa09b9dc17f8d4183892
Sergey Linev <S.Linev@gsi.de>
2022-09-29 13:22:05 +0200

    Date and build

--- build/jsroot.js
+++ build/jsroot.js
@@ -11,7 +11,7 @@ let version_id = "7.2.x";
 
 /** @summary version date
   * @desc Release date in format day/month/year like "19/11/2021" */
-let version_date = "5/09/2022";
+let version_date = "29/09/2022";
 
 /** @summary version id and date
   * @desc Produced by concatenation of {@link version_id} and {@link version_date}
@@ -81555,8 +81555,8 @@ class Polygon {
       return this;
    }
 
-   calculateProperties() {
-      if (this.normal) return;
+   calculateProperties(force) {
+      if (this.normal && !force) return;
 
       let a = this.vertices[0],
           b = this.vertices[1],
@@ -81905,8 +81905,7 @@ class Geometry {
                for (let n = 0; n < polygon.vertices.length; ++n)
                   polygon.vertices[n].applyMatrix4(transfer_matrix);
             }
-
-            polygon.calculateProperties();
+            polygon.calculateProperties(transfer_matrix);
          }
 
          this.tree = new Node( polygons, nodeid );
@@ -82133,8 +82132,7 @@ class Geometry {
             v.y *= y;
             v.z *= z;
          }
-         delete polygon.normal;
-         polygon.calculateProperties();
+         polygon.calculateProperties(true);
       }
    }
 
@@ -82195,7 +82193,6 @@ function createNormal(axis_name, pos, size) {
    }
 
    let polygon = new Polygon(vert);
-   polygon.calculateProperties();
 
    let node = new Node([polygon]);
 
