662ba4f648c94d4766de85a21ff5a8305d5f17c5
Sergey Linev <S.Linev@gsi.de>
2022-10-05 13:27:29 +0200

    Use single quotes in attributes handlers

--- modules/base/TAttFillHandler.mjs
+++ modules/base/TAttFillHandler.mjs
@@ -164,7 +164,7 @@ class TAttFillHandler {
          defs = svg.select('.canvas_defs');
 
       if (defs.empty())
-         defs = svg.insert("svg:defs", ":first-child").attr('class', "canvas_defs");
+         defs = svg.insert('svg:defs', ':first-child').attr('class', 'canvas_defs');
 
       this.pattern_url = `url(#${id})`;
       this.antialias = false;
@@ -172,47 +172,46 @@ class TAttFillHandler {
       if (!defs.select('.' + id).empty())
          return true;
 
-      let lines = "", lfill = null, fills = "", fills2 = "", w = 2, h = 2;
+      let lines = '', lfill = null, fills = '', fills2 = '', w = 2, h = 2;
 
       switch (this.pattern) {
-         case 3001: w = h = 2; fills = "M0,0h1v1h-1zM1,1h1v1h-1z"; break;
-         case 3002: w = 4; h = 2; fills = "M1,0h1v1h-1zM3,1h1v1h-1z"; break;
-         case 3003: w = h = 4; fills = "M2,1h1v1h-1zM0,3h1v1h-1z"; break;
-         case 3004: w = h = 8; lines = "M8,0L0,8"; break;
-         case 3005: w = h = 8; lines = "M0,0L8,8"; break;
-         case 3006: w = h = 4; lines = "M1,0v4"; break;
-         case 3007: w = h = 4; lines = "M0,1h4"; break;
+         case 3001: w = h = 2; fills = 'M0,0h1v1h-1zM1,1h1v1h-1z'; break;
+         case 3002: w = 4; h = 2; fills = 'M1,0h1v1h-1zM3,1h1v1h-1z'; break;
+         case 3003: w = h = 4; fills = 'M2,1h1v1h-1zM0,3h1v1h-1z'; break;
+         case 3004: w = h = 8; lines = 'M8,0L0,8'; break;
+         case 3005: w = h = 8; lines = 'M0,0L8,8'; break;
+         case 3006: w = h = 4; lines = 'M1,0v4'; break;
+         case 3007: w = h = 4; lines = 'M0,1h4'; break;
          case 3008:
             w = h = 10;
-            fills = "M0,3v-3h3ZM7,0h3v3ZM0,7v3h3ZM7,10h3v-3ZM5,2l3,3l-3,3l-3,-3Z";
-            lines = "M0,3l5,5M3,10l5,-5M10,7l-5,-5M7,0l-5,5";
+            fills = 'M0,3v-3h3ZM7,0h3v3ZM0,7v3h3ZM7,10h3v-3ZM5,2l3,3l-3,3l-3,-3Z';
+            lines = 'M0,3l5,5M3,10l5,-5M10,7l-5,-5M7,0l-5,5';
             break;
-         case 3009: w = 12; h = 12; lines = "M0,0A6,6,0,0,0,12,0M6,6A6,6,0,0,0,12,12M6,6A6,6,0,0,1,0,12"; lfill = 'none'; break;
-         case 3010: w = h = 10; lines = "M0,2h10M0,7h10M2,0v2M7,2v5M2,7v3"; break; // bricks
-         case 3011: w = 9; h = 18; lines = "M5,0v8M2,1l6,6M8,1l-6,6M9,9v8M6,10l3,3l-3,3M0,9v8M3,10l-3,3l3,3"; lfill = 'none'; break;
-         case 3012: w = 10; h = 20; lines = "M5,1A4,4,0,0,0,5,9A4,4,0,0,0,5,1M0,11A4,4,0,0,1,0,19M10,11A4,4,0,0,0,10,19"; lfill = 'none'; break;
-         case 3013: w = h = 7; lines = "M0,0L7,7M7,0L0,7"; lfill = 'none'; break;
-         case 3014: w = h = 16; lines = "M0,0h16v16h-16v-16M0,12h16M12,0v16M4,0v8M4,4h8M0,8h8M8,4v8"; lfill = 'none'; break;
-         case 3015: w = 6; h = 12; lines = "M2,1A2,2,0,0,0,2,5A2,2,0,0,0,2,1M0,7A2,2,0,0,1,0,11M6,7A2,2,0,0,0,6,11"; lfill = 'none'; break;
-         case 3016: w = 12; h = 7; lines = "M0,1A3,2,0,0,1,3,3A3,2,0,0,0,9,3A3,2,0,0,1,12,1"; lfill = 'none'; break;
-         case 3017: w = h = 4; lines = "M3,1l-2,2"; break;
-         case 3018: w = h = 4; lines = "M1,1l2,2"; break;
+         case 3009: w = 12; h = 12; lines = 'M0,0A6,6,0,0,0,12,0M6,6A6,6,0,0,0,12,12M6,6A6,6,0,0,1,0,12'; lfill = 'none'; break;
+         case 3010: w = h = 10; lines = 'M0,2h10M0,7h10M2,0v2M7,2v5M2,7v3'; break; // bricks
+         case 3011: w = 9; h = 18; lines = 'M5,0v8M2,1l6,6M8,1l-6,6M9,9v8M6,10l3,3l-3,3M0,9v8M3,10l-3,3l3,3'; lfill = 'none'; break;
+         case 3012: w = 10; h = 20; lines = 'M5,1A4,4,0,0,0,5,9A4,4,0,0,0,5,1M0,11A4,4,0,0,1,0,19M10,11A4,4,0,0,0,10,19'; lfill = 'none'; break;
+         case 3013: w = h = 7; lines = 'M0,0L7,7M7,0L0,7'; lfill = 'none'; break;
+         case 3014: w = h = 16; lines = 'M0,0h16v16h-16v-16M0,12h16M12,0v16M4,0v8M4,4h8M0,8h8M8,4v8'; lfill = 'none'; break;
+         case 3015: w = 6; h = 12; lines = 'M2,1A2,2,0,0,0,2,5A2,2,0,0,0,2,1M0,7A2,2,0,0,1,0,11M6,7A2,2,0,0,0,6,11'; lfill = 'none'; break;
+         case 3016: w = 12; h = 7; lines = 'M0,1A3,2,0,0,1,3,3A3,2,0,0,0,9,3A3,2,0,0,1,12,1'; lfill = 'none'; break;
+         case 3017: w = h = 4; lines = 'M3,1l-2,2'; break;
+         case 3018: w = h = 4; lines = 'M1,1l2,2'; break;
          case 3019:
             w = h = 12;
-            lines = "M1,6A5,5,0,0,0,11,6A5,5,0,0,0,1,6h-1h1A5,5,0,0,1,6,11v1v-1" +
-               "A5,5,0,0,1,11,6h1h-1A5,5,0,0,1,6,1v-1v1A5,5,0,0,1,1,6";
+            lines = 'M1,6A5,5,0,0,0,11,6A5,5,0,0,0,1,6h-1h1A5,5,0,0,1,6,11v1v-1A5,5,0,0,1,11,6h1h-1A5,5,0,0,1,6,1v-1v1A5,5,0,0,1,1,6';
             lfill = 'none';
             break;
-         case 3020: w = 7; h = 12; lines = "M1,0A2,3,0,0,0,3,3A2,3,0,0,1,3,9A2,3,0,0,0,1,12"; lfill = 'none'; break;
-         case 3021: w = h = 8; lines = "M8,2h-2v4h-4v2M2,0v2h-2"; lfill = 'none'; break; // left stairs
-         case 3022: w = h = 8; lines = "M0,2h2v4h4v2M6,0v2h2"; lfill = 'none'; break; // right stairs
-         case 3023: w = h = 8; fills = "M4,0h4v4zM8,4v4h-4z"; fills2 = "M4,0L0,4L4,8L8,4Z"; break;
-         case 3024: w = h = 16; fills = "M0,8v8h2v-8zM8,0v8h2v-8M4,14v2h12v-2z"; fills2 = "M0,2h8v6h4v-6h4v12h-12v-6h-4z"; break;
-         case 3025: w = h = 18; fills = "M5,13v-8h8ZM18,0v18h-18l5,-5h8v-8Z"; break;
+         case 3020: w = 7; h = 12; lines = 'M1,0A2,3,0,0,0,3,3A2,3,0,0,1,3,9A2,3,0,0,0,1,12'; lfill = 'none'; break;
+         case 3021: w = h = 8; lines = 'M8,2h-2v4h-4v2M2,0v2h-2'; lfill = 'none'; break; // left stairs
+         case 3022: w = h = 8; lines = 'M0,2h2v4h4v2M6,0v2h2'; lfill = 'none'; break; // right stairs
+         case 3023: w = h = 8; fills = 'M4,0h4v4zM8,4v4h-4z'; fills2 = 'M4,0L0,4L4,8L8,4Z'; break;
+         case 3024: w = h = 16; fills = 'M0,8v8h2v-8zM8,0v8h2v-8M4,14v2h12v-2z'; fills2 = 'M0,2h8v6h4v-6h4v12h-12v-6h-4z'; break;
+         case 3025: w = h = 18; fills = 'M5,13v-8h8ZM18,0v18h-18l5,-5h8v-8Z'; break;
          default:
             if ((this.pattern > 3025) && (this.pattern < 3100)) {
                // same as 3002, see TGX11.cxx, line 2234
-               w = 4; h = 2; fills = "M1,0h1v1h-1zM3,1h1v1h-1z"; break;
+               w = 4; h = 2; fills = 'M1,0h1v1h-1zM3,1h1v1h-1z'; break;
             }
 
             let code = this.pattern % 1000,
@@ -299,7 +298,7 @@ class TAttFillHandler {
       if (!fills && !lines) return false;
 
       let patt = defs.append('svg:pattern')
-                     .attr('id', id).attr('class', id).attr("patternUnits", "userSpaceOnUse")
+                     .attr('id', id).attr('class', id).attr('patternUnits', 'userSpaceOnUse')
                      .attr('width', w).attr('height', h);
 
       if (fills2) {
@@ -321,7 +320,7 @@ class TAttFillHandler {
 
       const sample = new TAttFillHandler({ svg, pattern: this.pattern, color: this.color, color_as_svg: true });
 
-      svg.append("path")
+      svg.append('path')
          .attr('d', `M0,0h${width}v${height}h${-width}z`)
          .call(sample.func);
    }
