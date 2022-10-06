649dae3e85e19f69f5f5bf569643ab9cbbcc991c
Sergey Linev <S.Linev@gsi.de>
2022-10-06 13:36:15 +0200

    Fix parseMulti implementation
    Original array must be parsed with standard JSON parser

--- modules/core.mjs
+++ modules/core.mjs
@@ -697,7 +697,7 @@ function parse(json) {
   * @returns {Array} array of parsed elements */
 function parseMulti(json) {
    if (!json) return null;
-   let arr = parse(json);
+   let arr = JSON.parse(json);
    if (arr && arr.length)
       for (let i = 0; i < arr.length; ++i)
          arr[i] = parse(arr[i]);
