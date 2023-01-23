00e57e3ec214b43124adba49e7edfe5473095c81
Sergey Linev <S.Linev@gsi.de>
2023-01-23 14:18:54 +0100

    Use same scatter pattern indexing for RH2

--- modules/hist2d/RH2Painter.mjs
+++ modules/hist2d/RH2Painter.mjs
@@ -1055,12 +1055,11 @@ class RH2Painter extends RHistPainter {
 
       for (colindx = 0; colindx < colPaths.length; ++colindx)
         if ((colPaths[colindx] !== undefined) && (colindx<cntr.length)) {
-           let pattern_class = 'scatter_' + colindx,
-               pattern = defs.select('.' + pattern_class);
+           let pattern_id = (this.pad_name || 'canv') + `_scatter_${colindx}`,
+               pattern = defs.select(`#${pattern_id}`);
            if (pattern.empty())
               pattern = defs.append('svg:pattern')
-                            .attr('class', pattern_class)
-                            .attr('id', 'jsroot_scatter_pattern_' + internals.id_counter++)
+                            .attr('id', pattern_id)
                             .attr('patternUnits','userSpaceOnUse');
            else
               pattern.selectAll('*').remove();
@@ -1097,7 +1096,7 @@ class RH2Painter extends RHistPainter {
            this.draw_g
                .append('svg:path')
                .attr('scatter-index', colindx)
-               .style('fill', `url(#${pattern.attr('id')})`)
+               .style('fill', `url(#${pattern_id})`)
                .attr('d', colPaths[colindx]);
         }
 
