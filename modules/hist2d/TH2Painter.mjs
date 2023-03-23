b2a048948186c051839a79cf21570fdac058be80
Sergey Linev <S.Linev@gsi.de>
2023-03-23 14:11:19 +0100

    Add possibility to specify custom projection kind using input

--- modules/hist2d/TH2Painter.mjs
+++ modules/hist2d/TH2Painter.mjs
@@ -188,11 +188,11 @@ class TH2Painter extends THistPainter {
    /** @summary Fill histogram context menu */
    fillHistContextMenu(menu) {
       if (!this.isTH2Poly()) {
-         menu.add('sub:Projections', () => this.toggleProjection());
          let kind = this.is_projection || '';
          if (kind) kind += this.projection_width;
+         menu.add('sub:Projections', () => menu.input('Input projection kind X1 or XY2', kind, 'string').then(val => this.toggleProjection(val)));
          const kinds = ['X1', 'X2', 'X3', 'X5', 'X10', 'Y1', 'Y2', 'Y3', 'Y5', 'Y10', 'XY1', 'XY2', 'XY3', 'XY5', 'XY10'];
-         if (this.is_projection) kinds.push('Off');
+         if (kind) kinds.unshift('Off');
          for (let k = 0; k < kinds.length; ++k)
             menu.addchk(kind==kinds[k], kinds[k], kinds[k], arg => this.toggleProjection(arg));
          menu.add('endsub:');
