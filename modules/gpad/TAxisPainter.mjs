5c25bf2a0eac265cef5501f1dae59b2392c1b1ca
Sergey Linev <S.Linev@gsi.de>
2023-03-27 17:09:13 +0200

    Small formatting

--- modules/gpad/TAxisPainter.mjs
+++ modules/gpad/TAxisPainter.mjs
@@ -1142,9 +1142,9 @@ class TAxisPainter extends ObjectPainter {
       this.lineatt.not_standard = true;
 
       if (!is_gaxis || (this.name === 'zaxis')) {
-         axis_g = layer.select('.' + this.name + '_container');
+         axis_g = layer.select(`.${this.name}_container`);
          if (axis_g.empty())
-            axis_g = layer.append('svg:g').attr('class',this.name + '_container');
+            axis_g = layer.append('svg:g').attr('class', `${this.name}_container`);
          else
             axis_g.selectAll('*').remove();
       }
