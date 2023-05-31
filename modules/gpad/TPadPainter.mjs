33279aa06f4d9ec79b30de2ddebf7b93b0d17c01
Sergey Linev <S.Linev@gsi.de>
2023-05-31 14:20:17 +0200

    Let disable only pad moving, allow resize with 3d objects

--- modules/gpad/TPadPainter.mjs
+++ modules/gpad/TPadPainter.mjs
@@ -673,7 +673,7 @@ class TPadPainter extends ObjectPainter {
 
       if (!this.iscan && !isBatchMode())
          addDragHandler(this, { x, y, width: w, height: h, no_transform: true,
-                                is_disabled: () => svg_can.property('pad_enlarged') || this.btns_active_flag || this._disable_dragging,
+                                is_disabled: kind => svg_can.property('pad_enlarged') || this.btns_active_flag || (this._disable_dragging && kind == 'move'),
                                 getDrawG: () => this.svg_this_pad(),
                                 pad_rect: { width, height },
                                 minwidth: 20, minheight: 20,
