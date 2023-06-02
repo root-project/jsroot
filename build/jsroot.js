5578e7e7c4f16628a50b4b5b55c56d39cd3e11f8
Sergey Linev <S.Linev@gsi.de>
2023-06-02 09:33:36 +0200

    build and log

--- build/jsroot.js
+++ build/jsroot.js
@@ -80370,7 +80370,8 @@ async function treeProcess(tree, selector, args) {
 
    if (!selector || !tree.$file || !selector.numBranches()) {
       if (selector) selector.Terminate(false);
-      return Promise.reject(Error('required parameter missing for TTree::Process'));
+      console.error('required parameter missing for TTree::Process');
+      return null;
    }
 
    // central handle with all information required for reading
@@ -80958,7 +80959,8 @@ async function treeProcess(tree, selector, args) {
 
       if (!item) {
          selector.Terminate(false);
-         return Promise.reject(Error(`Fail to add branch ${selector.nameOfBranch(nn)}`));
+         console.error(`Fail to add branch ${selector.nameOfBranch(nn)}`);
+         return null;
       }
    }
 
@@ -80985,7 +80987,8 @@ async function treeProcess(tree, selector, args) {
 
    if (handle.firstentry >= handle.lastentry) {
       selector.Terminate(false);
-      return Promise.reject(Error('No any common events for selected branches'));
+      console.error('No any common events for selected branches');
+      return null;
    }
 
    handle.process_min = handle.firstentry;
--- changes.md
+++ changes.md
@@ -3,6 +3,7 @@
 ## Changes in 7.3.x
 1. Fix - failure in normal_cdf calculation
 2. Fix - check in TTree::Draw for null buffer
+3. Fix - do not rise exception in treeProcess
 
 
 ## Changes in 7.3.2
