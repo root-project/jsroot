e89f9ab037e933fc8bcaa9f6ce1f7adcfcdf7e60
Sergey Linev <S.Linev@gsi.de>
2023-06-06 11:28:17 +0200

    ignore jpeg

--- demo/node/makepng.js
+++ demo/node/makepng.js
@@ -1,13 +1,32 @@
-import { version, openFile, makeSVG, svgToImage } from 'jsroot';
+import { version, openFile, makeSVG, makeImage, svgToImage } from 'jsroot';
 
 import { writeFileSync } from 'fs';
 
 console.log(`JSROOT version ${version}`);
 
 let file = await openFile('https://root.cern/js/files/hsimple.root');
-let obj = await file.readObject('hpxpy;1');
-let svg = await makeSVG({ object: obj, option: 'lego2', width: 1200, height: 800 });
-let buf = await svgToImage(svg, 'png', true);
+let hpxpy = await file.readObject('hpxpy;1');
 
-console.log(`create PNG image test.png size ${buf.byteLength}`);
-writeFileSync('test.png', buf);
+// testing 2D graphics
+let svg1 = await makeSVG({ object: hpxpy, option: 'col', width: 1200, height: 800 });
+let png1 = await makeImage({ format: 'png', object: hpxpy, option: 'col', width: 1200, height: 800 });
+let jpeg1 = await makeImage({ format: 'jpeg', object: hpxpy, option: 'col', width: 1200, height: 800 });
+let jpeg1buf = await makeImage({ format: 'jpeg', as_buffer: true, object: hpxpy, option: 'col', width: 1200, height: 800 });
+
+console.log(`Create test1.jpeg size ${jpeg1buf.byteLength}`);
+
+writeFileSync('test1.jpeg', jpeg1buf);
+
+
+//console.log(`create PNG image test.png size ${buf.byteLength}`);
+
+// testing 3D graphics
+let svg2 = await makeSVG({ object: hpxpy, option: 'lego2', width: 1200, height: 800 });
+let png2 = await makeImage({ format: 'png', object: hpxpy, option: 'lego2', width: 1200, height: 800 });
+let jpeg2 = await makeImage({ format: 'jpeg', object: hpxpy, option: 'lego2', width: 1200, height: 800 });
+let jpeg2buf = await makeImage({ format: 'jpeg', as_buffer: true, object: hpxpy, option: 'lego2', width: 1200, height: 800 });
+
+console.log(`Create test2.jpeg size ${jpeg2buf.byteLength}`);
+
+writeFileSync('test2.jpeg', jpeg2buf);
+
