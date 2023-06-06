1bf47f594be2d5759455754b7e8d328e6e878e20
Sergey Linev <S.Linev@gsi.de>
2023-06-06 12:26:05 +0200

    Rename image demos, extend make_image.js

--- demo/node/make_image.js
+++ demo/node/make_image.js
@@ -4,8 +4,11 @@ import { writeFileSync } from 'fs';
 
 console.log(`JSROOT version ${version}`);
 
+// loading data
 let file = await openFile('https://root.cern/js/files/hsimple.root');
 let hpxpy = await file.readObject('hpxpy;1');
+let file2 = await openFile('https://root.cern/js/files/geom/rootgeom.root');
+let geom = await file2.readObject('simple1;1');
 
 // testing 2D graphics
 let svg1 = await makeSVG({ object: hpxpy, option: 'col', width: 1200, height: 800 });
@@ -14,11 +17,8 @@ let jpeg1 = await makeImage({ format: 'jpeg', object: hpxpy, option: 'col', widt
 let jpeg1buf = await makeImage({ format: 'jpeg', as_buffer: true, object: hpxpy, option: 'col', width: 1200, height: 800 });
 
 console.log(`Create test1.jpeg size ${jpeg1buf.byteLength}`);
-
 writeFileSync('test1.jpeg', jpeg1buf);
 
-
-//console.log(`create PNG image test.png size ${buf.byteLength}`);
 
 // testing 3D graphics
 let svg2 = await makeSVG({ object: hpxpy, option: 'lego2', width: 1200, height: 800 });
@@ -27,6 +27,14 @@ let jpeg2 = await makeImage({ format: 'jpeg', object: hpxpy, option: 'lego2', wi
 let jpeg2buf = await makeImage({ format: 'jpeg', as_buffer: true, object: hpxpy, option: 'lego2', width: 1200, height: 800 });
 
 console.log(`Create test2.jpeg size ${jpeg2buf.byteLength}`);
-
 writeFileSync('test2.jpeg', jpeg2buf);
 
+
+// testing geometry
+let svg3 = await makeSVG({ object: geom, option: '', width: 1200, height: 800 });
+let png3 = await makeImage({ format: 'png', object: geom, option: '', width: 1200, height: 800 });
+let jpeg3 = await makeImage({ format: 'jpeg', object: geom, option: '', width: 1200, height: 800 });
+let jpeg3buf = await makeImage({ format: 'jpeg', as_buffer: true, object: geom, option: '', width: 1200, height: 800 });
+
+console.log(`Create test3.jpeg size ${jpeg3buf.byteLength}`);
+writeFileSync('test3.jpeg', jpeg3buf);
