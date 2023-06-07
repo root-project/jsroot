d646095130a5c7a4d5341ee52b77eb7af545dae1
Sergey Linev <S.Linev@gsi.de>
2023-06-07 10:19:19 +0200

    Fix problem creating SVG renderer in normal browser

--- modules/base/base3d.mjs
+++ modules/base/base3d.mjs
@@ -71,12 +71,17 @@ function createSVGRenderer(as_is, precision, doc) {
      }
    };
 
-   let originalDocument = globalThis.document;
-   globalThis.document = doc_wrapper;
+   let originalDocument;
+
+   if (isNodeJs()) {
+      originalDocument = globalThis.document;
+      globalThis.document = doc_wrapper;
+   }
 
    let rndr = new SVGRenderer();
 
-   globalThis.document = originalDocument;
+   if (isNodeJs())
+      globalThis.document = originalDocument;
 
    rndr.doc_wrapper = doc_wrapper; // use it to get final SVG code
 
