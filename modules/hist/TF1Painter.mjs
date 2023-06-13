c2dab6f65e579c6a345002edfd468fe0ef33741f
Sergey Linev <S.Linev@gsi.de>
2023-06-13 13:04:34 +0200

    Optimize TF1 painting

--- modules/hist/TF1Painter.mjs
+++ modules/hist/TF1Painter.mjs
@@ -11,7 +11,7 @@ function proivdeEvalPar(obj) {
 
    let _func = obj.fTitle, isformula = false, pprefix = '[';
    if (_func === 'gaus') _func = 'gaus(0)';
-   if (obj.fFormula && isStr(obj.fFormula.fFormula)) {
+   if (isStr(obj.fFormula?.fFormula)) {
      if (obj.fFormula.fFormula.indexOf('[](double*x,double*p)') == 0) {
         isformula = true; pprefix = 'p[';
         _func = obj.fFormula.fFormula.slice(21);
@@ -26,51 +26,49 @@ function proivdeEvalPar(obj) {
                parvalue = obj.fFormula.fClingParameters[pair.second];
            _func = _func.replace(regex, (parvalue < 0) ? `(${parvalue})` : parvalue);
         });
-  }
+   }
 
-  if ('formulas' in obj)
-     obj.formulas.forEach(entry => {
-       _func = _func.replaceAll(entry.fName, entry.fTitle);
-     });
+   obj.formulas?.forEach(entry => {
+      _func = _func.replaceAll(entry.fName, entry.fTitle);
+   });
 
-  _func = _func.replace(/\b(abs)\b/g, 'TMath::Abs')
-               .replace(/\b(TMath::Exp)/g, 'Math.exp')
-               .replace(/\b(TMath::Abs)/g, 'Math.abs');
+   _func = _func.replace(/\b(abs)\b/g, 'TMath::Abs')
+                .replace(/\b(TMath::Exp)/g, 'Math.exp')
+                .replace(/\b(TMath::Abs)/g, 'Math.abs')
+                .replace(/xygaus\(/g, 'this._math.gausxy(this, x, y, ')
+                .replace(/gaus\(/g, 'this._math.gaus(this, x, ')
+                .replace(/gausn\(/g, 'this._math.gausn(this, x, ')
+                .replace(/expo\(/g, 'this._math.expo(this, x, ')
+                .replace(/landau\(/g, 'this._math.landau(this, x, ')
+                .replace(/landaun\(/g, 'this._math.landaun(this, x, ')
+                .replace(/TMath::/g, 'this._math.')
+                .replace(/ROOT::Math::/g, 'this._math.');
 
-  _func = _func.replace(/xygaus\(/g, 'this._math.gausxy(this, x, y, ')
-               .replace(/gaus\(/g, 'this._math.gaus(this, x, ')
-               .replace(/gausn\(/g, 'this._math.gausn(this, x, ')
-               .replace(/expo\(/g, 'this._math.expo(this, x, ')
-               .replace(/landau\(/g, 'this._math.landau(this, x, ')
-               .replace(/landaun\(/g, 'this._math.landaun(this, x, ')
-               .replace(/TMath::/g, 'this._math.')
-               .replace(/ROOT::Math::/g, 'this._math.');
+   for (let i = 0; i < obj.fNpar; ++i)
+      _func = _func.replaceAll(pprefix + i + ']', `(${obj.GetParValue(i)})`);
 
-  for (let i = 0; i < obj.fNpar; ++i)
-    _func = _func.replaceAll(pprefix + i + ']', `(${obj.GetParValue(i)})`);
+   _func = _func.replace(/\b(sin)\b/gi, 'Math.sin')
+                .replace(/\b(cos)\b/gi, 'Math.cos')
+                .replace(/\b(tan)\b/gi, 'Math.tan')
+                .replace(/\b(exp)\b/gi, 'Math.exp')
+                .replace(/\b(log10)\b/gi, 'Math.log10')
+                .replace(/\b(pow)\b/gi, 'Math.pow')
+                .replace(/pi/g, 'Math.PI');
+   for (let n = 2; n < 10; ++n)
+      _func = _func.replaceAll(`x^${n}`, `Math.pow(x,${n})`);
 
-  _func = _func.replace(/\b(sin)\b/gi, 'Math.sin')
-               .replace(/\b(cos)\b/gi, 'Math.cos')
-               .replace(/\b(tan)\b/gi, 'Math.tan')
-               .replace(/\b(exp)\b/gi, 'Math.exp')
-               .replace(/\b(log10)\b/gi, 'Math.log10')
-               .replace(/\b(pow)\b/gi, 'Math.pow')
-               .replace(/pi/g, 'Math.PI');
-  for (let n = 2; n < 10; ++n)
-     _func = _func.replaceAll(`x^${n}`, `Math.pow(x,${n})`);
-
-  if (isformula) {
-     _func = _func.replace(/x\[0\]/g,'x');
-     if (obj._typename === clTF2) {
-        _func = _func.replace(/x\[1\]/g,'y');
-        obj.evalPar = new Function('x', 'y', _func).bind(obj);
-     } else {
-        obj.evalPar = new Function('x', _func).bind(obj);
-     }
-  } else if (obj._typename === clTF2)
-     obj.evalPar = new Function('x', 'y', 'return ' + _func).bind(obj);
-  else
-     obj.evalPar = new Function('x', 'return ' + _func).bind(obj);
+   if (isformula) {
+      _func = _func.replace(/x\[0\]/g,'x');
+      if (obj._typename === clTF2) {
+         _func = _func.replace(/x\[1\]/g,'y');
+         obj.evalPar = new Function('x', 'y', _func).bind(obj);
+      } else {
+         obj.evalPar = new Function('x', _func).bind(obj);
+      }
+   } else if (obj._typename === clTF2)
+      obj.evalPar = new Function('x', 'y', 'return ' + _func).bind(obj);
+   else
+      obj.evalPar = new Function('x', 'return ' + _func).bind(obj);
 }
 
 /**
@@ -138,7 +136,7 @@ class TF1Painter extends ObjectPainter {
 
          np = tf1.fSave.length - 2;
          xmin = tf1.fSave[np];
-         xmax = tf1.fSave[np+1];
+         xmax = tf1.fSave[np + 1];
          res = [];
          dx = 0;
          let use_histo = tf1.$histo && (xmin === xmax), bin = 0;
