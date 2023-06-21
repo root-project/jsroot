1f569a7aca8283ccd1dab0cdb446ebfec641da27
Sergey Linev <S.Linev@gsi.de>
2023-06-21 13:23:59 +0200

    Use last valid pad canvas pixel size if current size is too small

--- modules/gpad/TPadPainter.mjs
+++ modules/gpad/TPadPainter.mjs
@@ -516,7 +516,15 @@ class TPadPainter extends ObjectPainter {
       if ((rect.width <= lmt) || (rect.height <= lmt)) {
          svg.style('display', 'none');
          console.warn(`Hide canvas while geometry too small w=${rect.width} h=${rect.height}`);
-         rect.width = 200; rect.height = 100; // just to complete drawing
+         if (this._pad_width && this._pad_height) {
+            // use last valid dimensions
+            rect.width = this._pad_width;
+            rect.height = this._pad_height;
+         } else {
+            // just to complete drawing.
+            rect.width = 800;
+            rect.height = 600;
+         }
       } else {
          svg.style('display', null);
       }
