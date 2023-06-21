6c8905fa4b67b8d404d02006fb444e4a778058e5
Sergey Linev <S.Linev@gsi.de>
2023-06-21 13:35:04 +0200

    Add simple python3 server for testing JSROOT to repository

---
+++ server/Readme.md
@@ -0,0 +1,29 @@
+# Simple http server for JSROOT
+
+Can be used to serve JSROOT and ROOT files on localhost httpserver.
+
+1. Checkout jsroot in working directory like `~/work`:
+
+    [shell] mkdir ~/work
+    [shell] cd ~/work
+    [shell] git clone https://github.com/root-project/jsroot.git
+
+2. Copy some ROOT files to `files` sub-directory:
+
+    [shell] mkdir ~/work/files
+    [shell] cp $ROOTSYS/tutorials/hsimple.root ~/work/files/
+
+3. Start server from working directory:
+
+    [shell] cd ~/work
+    [shell] python3 jsroot/server/server.py
+
+4. Open ROOT file in JSROOT with the link:
+
+     http://localhost:8000/jsroot/?file=files/hsimple.root&item=hpxpy&opt=colz
+
+5. Also test examples.htm and api.htm:
+
+     http://localhost:8000/jsroot/examples.htm
+     http://localhost:8000/jsroot/api.htm
+
---
+++ server/server.py
@@ -0,0 +1,120 @@
+# taken from https://github.com/danvk/RangeHTTPServer
+
+from http import server # Python 3
+
+from http.server import SimpleHTTPRequestHandler
+
+import os
+import re
+
+
+def copy_byte_range(infile, outfile, start=None, stop=None, bufsize=16*1024):
+    '''Like shutil.copyfileobj, but only copy a range of the streams.
+    Both start and stop are inclusive.
+    '''
+    if start is not None: infile.seek(start)
+    while 1:
+        to_read = min(bufsize, stop + 1 - infile.tell() if stop else bufsize)
+        buf = infile.read(to_read)
+        if not buf:
+            break
+        outfile.write(buf)
+
+
+BYTE_RANGE_RE = re.compile(r'bytes=(\d+)-(\d+)?$')
+def parse_byte_range(byte_range):
+    '''Returns the two numbers in 'bytes=123-456' or throws ValueError.
+    The last number or both numbers may be None.
+    '''
+    if byte_range.strip() == '':
+        return None, None
+
+    m = BYTE_RANGE_RE.match(byte_range)
+    if not m:
+        raise ValueError('Invalid byte range %s' % byte_range)
+
+    first, last = [x and int(x) for x in m.groups()]
+    if last and last < first:
+        raise ValueError('Invalid byte range %s' % byte_range)
+    return first, last
+
+
+class RangeRequestHandler(SimpleHTTPRequestHandler):
+    """Adds support for HTTP 'Range' requests to SimpleHTTPRequestHandler
+    The approach is to:
+    - Override send_head to look for 'Range' and respond appropriately.
+    - Override copyfile to only transmit a range when requested.
+    """
+
+    def translate_path(self, path):
+        res = SimpleHTTPRequestHandler.translate_path(self, path)
+        p = res.find('?stamp')
+        if p > 0:
+           res = res[0:p]
+        return res
+
+
+    def send_head(self):
+        if 'Range' not in self.headers:
+            self.range = None
+            return SimpleHTTPRequestHandler.send_head(self)
+        try:
+            self.range = parse_byte_range(self.headers['Range'])
+        except ValueError as e:
+            self.send_error(400, 'Invalid byte range')
+            return None
+        first, last = self.range
+
+        # Mirroring SimpleHTTPServer.py here
+        path = self.translate_path(self.path)
+        f = None
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except IOError:
+            self.send_error(404, 'File not found')
+            return None
+
+        fs = os.fstat(f.fileno())
+        file_len = fs[6]
+        if first >= file_len:
+            self.send_error(416, 'Requested Range Not Satisfiable')
+            return None
+
+        self.send_response(206)
+        self.send_header('Content-type', ctype)
+        self.send_header('Accept-Ranges', 'bytes')
+
+        if last is None or last >= file_len:
+            last = file_len - 1
+        response_length = last - first + 1
+
+        self.send_header('Content-Range',
+                         'bytes %s-%s/%s' % (first, last, file_len))
+        self.send_header('Content-Length', str(response_length))
+        self.send_header('Last-Modified', self.date_time_string(fs.st_mtime))
+        self.end_headers()
+        return f
+
+    def copyfile(self, source, outputfile):
+        if not self.range:
+            return SimpleHTTPRequestHandler.copyfile(self, source, outputfile)
+
+        # SimpleHTTPRequestHandler uses shutil.copyfileobj, which doesn't let
+        # you stop the copying before the end of the file.
+        start, stop = self.range  # set in send_head()
+        copy_byte_range(source, outputfile, start, stop)
+
+    def end_headers(self):
+        self.send_my_headers()
+        SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header('Accept-Ranges', 'bytes')
+        self.send_header("Access-Control-Allow-Origin", "*")
+        if self.path.count(".json.gz") == 1 and (self.path.count("?") == 0 or self.path.count("?compact") == 1 or self.path.count("?method") == 1):
+           self.send_header("Content-Encoding", "gzip")
+
+
+if __name__ == '__main__':
+    server.test(HandlerClass=RangeRequestHandler)
