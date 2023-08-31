761c173125fb556425cebb2fd7ac1c76a966b38b
Sergey Linev <S.Linev@gsi.de>
2023-08-31 14:15:05 +0200

    Fix typo

--- modules/geom/TGeoPainter.mjs
+++ modules/geom/TGeoPainter.mjs
@@ -3879,7 +3879,7 @@ class TGeoPainter extends ObjectPainter {
 
             SetMaxVisNodes: limit => {
                if (!this.ctrl.maxnodes)
-                  this.ctrl.maxnodes = pasrseInt(limit) || 0;
+                  this.ctrl.maxnodes = parseInt(limit) || 0;
             },
 
             SetVisLevel: limit => {
