2b2029144ff4d17c51fc10618b59a4b2b38bf927
Sergey Linev <S.Linev@gsi.de>
2023-08-31 16:10:32 +0200

    Fix some important warnings in hist classes

--- modules/hist/RH3Painter.mjs
+++ modules/hist/RH3Painter.mjs
@@ -433,8 +433,8 @@ class RH3Painter extends RHistPainter {
       }
 
       let binx, grx, biny, gry, binz, grz;
-      xaxis = this.getAxis('x'),
-      yaxis = this.getAxis('y'),
+      xaxis = this.getAxis('x');
+      yaxis = this.getAxis('y');
       zaxis = this.getAxis('z');
 
       for (i = i1; i < i2; i += di) {
@@ -516,10 +516,10 @@ class RH3Painter extends RHistPainter {
 
          if (use_colors) fillcolor = palette.getColor(ncol);
 
-         let material = use_lambert ? new MeshLambertMaterial({ color: fillcolor, opacity: use_opacity, transparent: use_opacity < 1, vertexColors: false })
-                                    : new MeshBasicMaterial({ color: fillcolor, opacity: use_opacity, transparent: use_opacity < 1, vertexColors: false });
-
-         let combined_bins = new Mesh(all_bins_buffgeom, material);
+         const material = use_lambert
+                           ? new MeshLambertMaterial({ color: fillcolor, opacity: use_opacity, transparent: use_opacity < 1, vertexColors: false })
+                           : new MeshBasicMaterial({ color: fillcolor, opacity: use_opacity, transparent: use_opacity < 1, vertexColors: false }),
+               combined_bins = new Mesh(all_bins_buffgeom, material);
 
          combined_bins.bins = bin_tooltips[nseq];
          combined_bins.bins_faces = buffer_size/9;
@@ -575,19 +575,17 @@ class RH3Painter extends RHistPainter {
    }
 
    draw3D() {
-
       if (!this.draw_content)
          return false;
 
-      //this.options.Scatter = false;
-      //this.options.Box = true;
+      // this.options.Scatter = false;
+      // this.options.Box = true;
 
-      let handle = this.prepareDraw({ only_indexes: true, extra: -0.5, right_extra: -1 });
-
-      let pr = this.options.Scatter ? this.draw3DScatter(handle) : Promise.resolve(false);
+      const handle = this.prepareDraw({ only_indexes: true, extra: -0.5, right_extra: -1 }),
+            pr = this.options.Scatter ? this.draw3DScatter(handle) : Promise.resolve(false);
 
       return pr.then(res => {
-         return res ? res : this.draw3DBins(handle);
+         return res || this.draw3DBins(handle);
       });
    }
 
