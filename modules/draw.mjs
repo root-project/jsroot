6d165145d42041cec322058bd1012c30b90bdab6
Sergey Linev <S.Linev@gsi.de>
2023-09-05 11:06:35 +0200

    Implement simple zooming tests

--- modules/draw.mjs
+++ modules/draw.mjs
@@ -632,16 +632,37 @@ async function makeImage(args) {
           : build(d3_select('body').append('div').style('display', 'none'));
 }
 
+
+/** @summary test zooming features
+  * @private */
+async function $testZooming(node) {
+   const cp = getElementCanvPainter(node);
+   if (!cp) return;
+
+   const fp = cp.getFramePainter();
+   if (typeof fp?.zomm !== 'function') return;
+   if (typeof fp.scale_xmin === 'undefined' || typeof fp.scale_ymax === 'undefined') return;
+
+   const xmin = fp.scale_xmin, xmax = fp.scale_xmax, ymin = fp.scale_yxmin, ymax = fp.scale_ymax;
+
+   return fp.zoom(xmin + 0.2*(xmax - xmin), xmin + 0.8*(xmax - xmin), ymin + 0.2*(ymax - ymin), ymin + 0.8*(ymax - ymin))
+            .then(() => fp.unzoom())
+            .then(() => fp.zoom('x', xmin + 0.22*(xmax - xmin), xmin + 0.25*(xmax - xmin)))
+            .then(() => fp.zoom('y', ymin + 0.12*(ymax - ymin), ymin + 0.13*(ymax - ymin)))
+            .then(() => fp.unzoom())
+}
+
 /** @summary test interactive features of JSROOT drawings
   * @desc used in https://github.com/linev/jsroot-test
   * @private */
-
 function testInteractivity(args) {
    async function build(main) {
       main.attr('width', args.width).attr('height', args.height)
           .style('width', args.width + 'px').style('height', args.height + 'px');
 
       return draw(main.node(), args.object, args.option || '').then(() => {
+         return $testZooming(main.node());
+      }).then(() => {
          cleanup(main.node());
          main.remove();
          return true;
