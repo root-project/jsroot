7385ef8db98d1303f24a2ffde37c4e0ddee5d1bc
Sergey Linev <S.Linev@gsi.de>
2023-09-05 16:12:46 +0200

    Handle timeout in interactive testing
    Let observe changes, enabled only in testing.htm

--- modules/testing.mjs
+++ modules/testing.mjs
@@ -1,12 +1,21 @@
 import { isNodeJs, isBatchMode, setBatchMode } from './core.mjs';
 
-import { select as d3_select, pointer as d3_pointer } from './d3.mjs';
+import { select as d3_select } from './d3.mjs';
 
 import { _loadJSDOM } from './base/BasePainter.mjs';
 
 import { cleanup, getElementCanvPainter } from './base/ObjectPainter.mjs';
 
 import { draw } from './draw.mjs';
+
+async function _test_timeout(args, portion = 1) {
+   if (!args?.timeout)
+      return true;
+
+   return new Promise(resolve => {
+      setTimeout(resolve, Math.round(portion * args.timeout));
+   });
+}
 
 class EmulationMouseEvent {
 
@@ -31,7 +40,7 @@ class EmulationMouseEvent {
 
 /** @summary test zooming features
   * @private */
-async function testZooming(node) {
+async function testZooming(node, args) {
    const cp = getElementCanvPainter(node);
    if (!cp) return;
 
@@ -43,15 +52,19 @@ async function testZooming(node) {
    const xmin = fp.scale_xmin, xmax = fp.scale_xmax, ymin = fp.scale_yxmin, ymax = fp.scale_ymax;
 
    return fp.zoom(xmin + 0.2*(xmax - xmin), xmin + 0.8*(xmax - xmin), ymin + 0.2*(ymax - ymin), ymin + 0.8*(ymax - ymin))
+            .then(() => _test_timeout(args))
             .then(() => fp.unzoom())
+            .then(() => _test_timeout(args))
             .then(() => fp.zoomSingle('x', xmin + 0.22*(xmax - xmin), xmin + 0.25*(xmax - xmin)))
-            .then(() => fp.zoomSingle('y', ymin + 0.12*(ymax - ymin), ymin + 0.13*(ymax - ymin)))
+            .then(() => _test_timeout(args))
+            .then(() => fp.zoomSingle('y', ymin + 0.12*(ymax - ymin), ymin + 0.43*(ymax - ymin)))
+            .then(() => _test_timeout(args))
             .then(() => fp.unzoom())
 }
 
 /** @summary test zooming features
   * @private */
-async function testMouseZooming(node) {
+async function testMouseZooming(node, args) {
    const cp = getElementCanvPainter(node),
          fp = cp?.getFramePainter();
 
@@ -61,18 +74,23 @@ async function testMouseZooming(node) {
    const fw = fp.getFrameWidth(), fh = fp.getFrameHeight(),
          evnt = new EmulationMouseEvent();
 
-   evnt.set(fw*0.2, fh*0.2);
-
+   evnt.set(fw*0.1, fh*0.1);
    fp.startRectSel(evnt);
+
+   await _test_timeout(args);
+
    for (let i = 2; i < 10; ++i) {
       evnt.set(fw*0.1*i, fh*0.1*i);
       fp.moveRectSel(evnt);
+      await _test_timeout(args, 0.2);
    }
-   return fp.endRectSel(evnt).then(() => fp.unzoom());
+   return Promise.all([fp.endRectSel(evnt)])
+            .then(() => _test_timeout(args))
+            .then(() => fp.unzoom());
 }
 
-async function do_testing(dom) {
-   return testZooming(dom).then(() => testMouseZooming(dom));
+async function _testing(dom, args) {
+   return testZooming(dom, args).then(() => testMouseZooming(dom, args));
 }
 
 
@@ -81,7 +99,7 @@ async function do_testing(dom) {
   * @private */
 async function testInteractivity(args) {
    if (args.dom)
-      return do_testing(args.dom);
+      return _testing(args.dom, args);
 
    async function build(main) {
       main.attr('width', args.width).attr('height', args.height)
@@ -103,7 +121,7 @@ async function testInteractivity(args) {
       setBatchMode(false);
 
       return draw(main.node(), args.object, args.option || '')
-             .then(() => do_testing(main.node()))
+             .then(() => _testing(main.node(), args))
              .then(() => {
                 cleanup(main.node());
                 main.remove();
