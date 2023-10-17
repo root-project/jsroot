abe19c4b2f005430e4ed2c9dde39baf94226fc26
Sergey Linev <S.Linev@gsi.de>
2023-10-17 12:58:56 +0200

    Also support skewdx for 1dim histogram

--- modules/hist2d/TH1Painter.mjs
+++ modules/hist2d/TH1Painter.mjs
@@ -168,9 +168,10 @@ class TH1Painter extends THistPainter {
             left = this.getSelectIndex('x', 'left'),
             right = this.getSelectIndex('x', 'right'),
             fp = this.getFramePainter(),
-            res = { name: histo.fName, meanx: 0, meany: 0, rmsx: 0, rmsy: 0, integral: 0, entries: this.stat_entries, xmax: 0, wmax: 0, skewx: 0 },
+            res = { name: histo.fName, meanx: 0, meany: 0, rmsx: 0, rmsy: 0, integral: 0,
+                    entries: this.stat_entries, eff_entries: 0, xmax: 0, wmax: 0, skewx: 0, skewdx: 0 },
             has_counted_stat = !fp.isAxisZoomed('x') && (Math.abs(histo.fTsumw) > 1e-300);
-      let stat_sumw = 0, stat_sumwx = 0, stat_sumwx2 = 0, stat_sumwy = 0, stat_sumwy2 = 0,
+      let stat_sumw = 0, stat_sumw2 = 0, stat_sumwx = 0, stat_sumwx2 = 0, stat_sumwy = 0, stat_sumwy2 = 0,
           i, xx = 0, w = 0, xmax = null, wmax = null;
 
       for (i = left; i < right; ++i) {
@@ -193,6 +194,7 @@ class TH1Painter extends THistPainter {
 
          if (!has_counted_stat) {
             stat_sumw += w;
+            stat_sumw2 += w * w;
             stat_sumwx += w * xx;
             stat_sumwx2 += w * xx**2;
          }
@@ -201,11 +203,14 @@ class TH1Painter extends THistPainter {
       // when no range selection done, use original statistic from histogram
       if (has_counted_stat) {
          stat_sumw = histo.fTsumw;
+         stat_sumw2 = histo.fTsumw2;
          stat_sumwx = histo.fTsumwx;
          stat_sumwx2 = histo.fTsumwx2;
       }
 
       res.integral = stat_sumw;
+
+      res.eff_entries = stat_sumw2 ? stat_sumw*stat_sumw/stat_sumw2 : Math.abs(stat_sumw);
 
       if (Math.abs(stat_sumw) > 1e-300) {
          res.meanx = stat_sumwx / stat_sumw;
@@ -232,6 +237,7 @@ class TH1Painter extends THistPainter {
          const stddev3 = res.rmsx * res.rmsx * res.rmsx;
          if (np * stddev3 !== 0)
             res.skewx = sum / (np * stddev3);
+         res.skewdx = res.eff_entries > 0 ? Math.sqrt(6/res.eff_entries) : 0;
       }
 
       return res;
@@ -295,8 +301,10 @@ class TH1Painter extends THistPainter {
          if (print_integral > 0)
             stat.addText('Integral = ' + stat.format(data.integral, 'entries'));
 
-         if (print_skew > 0)
-            stat.addText('Skew = ' + stat.format(data.skewx));
+         if (print_skew === 2)
+            stat.addText(`Skew = ${stat.format(data.skewx)} #pm ${stat.format(data.skewdx)}`);
+         else if (print_skew > 0)
+            stat.addText(`Skew = ${stat.format(data.skewx)}`);
 
          if (print_kurt > 0)
             stat.addText('Kurt = <not avail>');
