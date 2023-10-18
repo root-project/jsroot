09d5ae2e0d71c5befbf639526e2b54560591339c
Sergey Linev <S.Linev@gsi.de>
2023-10-18 13:02:48 +0200

    Improve hpainter item click
    If same object clicked second time, it not automatically expand.
    Only special classes like TGeoManager will do so

--- modules/gui/HierarchyPainter.mjs
+++ modules/gui/HierarchyPainter.mjs
@@ -1624,7 +1624,7 @@ class HierarchyPainter extends BasePainter {
          if (can_draw && can_expand && !drawopt) {
             // if default action specified as expand, disable drawing
             // if already displayed, try to expand
-            if (dflt_expand || (handle?.dflt === 'expand') || this.isItemDisplayed(itemname)) can_draw = false;
+            if (dflt_expand || (handle?.dflt === 'expand') || (handle?.exapnd_after_draw && this.isItemDisplayed(itemname))) can_draw = false;
          }
 
          if (can_draw && !drawopt)
