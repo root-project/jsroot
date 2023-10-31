8688a21bc59675cfbe3a4b3b7023d470d4f45908
Sergey Linev <S.Linev@gsi.de>
2023-10-31 08:41:53 +0100

    Date and build

--- build/jsroot.js
+++ build/jsroot.js
@@ -11,7 +11,7 @@ const version_id = 'dev',
 
 /** @summary version date
   * @desc Release date in format day/month/year like '14/04/2022' */
-version_date = '30/10/2023',
+version_date = '31/10/2023',
 
 /** @summary version id and date
   * @desc Produced by concatenation of {@link version_id} and {@link version_date}
@@ -1038,7 +1038,7 @@ const prROOT = 'ROOT.', clTObject = 'TObject', clTNamed = 'TNamed', clTString = 
       clTAttPad = 'TAttPad', clTPad = 'TPad', clTCanvas = 'TCanvas', clTAttCanvas = 'TAttCanvas',
       clTGaxis = 'TGaxis', clTAttAxis = 'TAttAxis', clTAxis = 'TAxis', clTStyle = 'TStyle',
       clTH1 = 'TH1', clTH1I = 'TH1I', clTH1D = 'TH1D', clTH2 = 'TH2', clTH2I = 'TH2I', clTH2F = 'TH2F', clTH3 = 'TH3',
-      clTF1 = 'TF1', clTF2 = 'TF2', clTProfile = 'TProfile', clTProfile2D = 'TProfile2D', clTProfile3D = 'TProfile3D',
+      clTF1 = 'TF1', clTF2 = 'TF2', clTF3 = 'TF3', clTProfile = 'TProfile', clTProfile2D = 'TProfile2D', clTProfile3D = 'TProfile3D',
       clTGeoVolume = 'TGeoVolume', clTGeoNode = 'TGeoNode', clTGeoNodeMatrix = 'TGeoNodeMatrix',
       nsREX = 'ROOT::Experimental::',
       kNoZoom = -1111, kNoStats = BIT(9), kInspect = 'inspect';
@@ -1880,6 +1880,7 @@ clTCutG: clTCutG,
 clTDiamond: clTDiamond,
 clTF1: clTF1,
 clTF2: clTF2,
+clTF3: clTF3,
 clTFile: clTFile,
 clTGaxis: clTGaxis,
 clTGeoNode: clTGeoNode,
@@ -80007,7 +80008,7 @@ class TH3Painter extends THistPainter {
       }
 
 
-      if (dofit) stat.fillFunctionStat(this.findFunction('TF3'), dofit, 3);
+      if (dofit) stat.fillFunctionStat(this.findFunction(clTF3), dofit, 3);
 
       return true;
    }
@@ -100423,6 +100424,7 @@ drawFuncs = { lst: [
    { name: clTObjString, icon: 'img_text', func: drawRawText },
    { name: clTF1, icon: 'img_tf1', class: () => Promise.resolve().then(function () { return TF1Painter$1; }).then(h => h.TF1Painter) },
    { name: clTF2, icon: 'img_tf2', class: () => Promise.resolve().then(function () { return TF2Painter$1; }).then(h => h.TF2Painter), opt: ';BOX;ARR;SURF;SURF1;SURF2;SURF4;SURF6;LEGO;LEGO0;LEGO1;LEGO2;LEGO3;LEGO4;same' },
+   { name: clTF3, icon: 'img_histo3d', class: () => Promise.resolve().then(function () { return TF3Painter$1; }).then(h => h.TF3Painter), opt: ';SURF' },
    { name: clTSpline3, icon: 'img_tf1', class: () => Promise.resolve().then(function () { return TSplinePainter$1; }).then(h => h.TSplinePainter) },
    { name: 'TSpline5', sameas: clTSpline3 },
    { name: clTEllipse, icon: 'img_graph', draw: () => import_more().then(h => h.drawEllipse), direct: true },
@@ -110432,12 +110434,18 @@ function proivdeEvalPar(obj) {
 
    if (isformula) {
       _func = _func.replace(/x\[0\]/g, 'x');
-      if (obj._typename === clTF2) {
+      if (obj._typename === clTF3) {
+         _func = _func.replace(/x\[1\]/g, 'y');
+         _func = _func.replace(/x\[2\]/g, 'z');
+         obj.evalPar = new Function('x', 'y', 'z', _func).bind(obj);
+      } else if (obj._typename === clTF2) {
          _func = _func.replace(/x\[1\]/g, 'y');
          obj.evalPar = new Function('x', 'y', _func).bind(obj);
       } else
          obj.evalPar = new Function('x', _func).bind(obj);
-   } else if (obj._typename === clTF2)
+   } else if (obj._typename === clTF3)
+      obj.evalPar = new Function('x', 'y', 'z', 'return ' + _func).bind(obj);
+   else if (obj._typename === clTF2)
       obj.evalPar = new Function('x', 'y', 'return ' + _func).bind(obj);
    else
       obj.evalPar = new Function('x', 'return ' + _func).bind(obj);
@@ -110532,8 +110540,8 @@ class TF1Painter extends TH1Painter$2 {
          if (hist.fNcells !== num + 2) {
             hist.fNcells = num + 2;
             hist.fArray = new Float32Array(hist.fNcells);
-            hist.fArray.fill(0);
          }
+         hist.fArray.fill(0);
          hist.fXaxis.fNbins = num;
          hist.fXaxis.fXbins = [];
       };
@@ -111972,8 +111980,8 @@ class TF2Painter extends TH2Painter {
          if (hist.fNcells !== (nx + 2) * (ny + 2)) {
             hist.fNcells = (nx + 2) * (ny + 2);
             hist.fArray = new Float32Array(hist.fNcells);
-            hist.fArray.fill(0);
          }
+         hist.fArray.fill(0);
          hist.fXaxis.fNbins = nx;
          hist.fXaxis.fXbins = [];
          hist.fYaxis.fNbins = ny;
@@ -112225,6 +112233,351 @@ class TF2Painter extends TH2Painter {
 var TF2Painter$1 = /*#__PURE__*/Object.freeze({
 __proto__: null,
 TF2Painter: TF2Painter
+});
+
+function findZValue(arrz, arrv, cross = 0) {
+   for (let i = 0; i < arrz.length - 1; ++i) {
+      const v1 = arrv[i], v2 = arrv[i+1],
+            z1 = arrz[i], z2 = arrz[i+1];
+      if (v1 === cross) return z1;
+      if (v2 === cross) return z2;
+      if ((v1 < cross) !== (v2 < cross))
+         return z1 + (cross - v1) / (v2 - v1) * (z2 - z1);
+   }
+
+   return arrz[0] - 1;
+}
+
+
+/**
+  * @summary Painter for TF3 object
+  *
+  * @private
+  */
+
+class TF3Painter extends TH2Painter {
+
+   /** @summary Returns drawn object name */
+   getObjectName() { return this.$func?.fName ?? 'func'; }
+
+   /** @summary Returns drawn object class name */
+   getClassName() { return this.$func?._typename ?? clTF3; }
+
+   /** @summary Returns true while function is drawn */
+   isTF1() { return true; }
+
+   /** @summary Update histogram */
+   updateObject(obj /*, opt */) {
+      if (!obj || (this.getClassName() !== obj._typename)) return false;
+      delete obj.evalPar;
+      const histo = this.getHisto();
+
+      if (this.webcanv_hist) {
+         const h0 = this.getPadPainter()?.findInPrimitives('Func', clTH2F);
+         if (h0) this.updateAxes(histo, h0, this.getFramePainter());
+      }
+
+      this.$func = obj;
+      this.createTF3Histogram(obj, histo);
+      this.scanContent();
+      return true;
+   }
+
+   /** @summary Redraw TF2
+     * @private */
+   redraw(reason) {
+      if (!this._use_saved_points && (reason === 'logx' || reason === 'logy' || reason === 'logy' || reason === 'zoom')) {
+         this.createTF3Histogram(this.$func, this.getHisto());
+         this.scanContent();
+      }
+
+      return super.redraw(reason);
+   }
+
+   /** @summary Create histogram for TF2 drawing
+     * @private */
+   createTF3Histogram(func, hist = undefined) {
+      const nsave = func.fSave.length - 9;
+
+      this._use_saved_points = (nsave > 0) && (settings.PreferSavedPoints || this.force_saved);
+
+      const fp = this.getFramePainter(),
+            pad = this.getPadPainter()?.getRootPad(true),
+            logx = pad?.fLogx, logy = pad?.fLogy,
+            gr = fp?.getGrFuncs(this.second_x, this.second_y);
+      let xmin = func.fXmin, xmax = func.fXmax,
+          ymin = func.fYmin, ymax = func.fYmax,
+          zmin = func.fZmin, zmax = func.fZmax;
+
+     if (gr?.zoom_xmin !== gr?.zoom_xmax) {
+         xmin = Math.min(xmin, gr.zoom_xmin);
+         xmax = Math.max(xmax, gr.zoom_xmax);
+      }
+
+     if (gr?.zoom_ymin !== gr?.zoom_ymax) {
+         ymin = Math.min(ymin, gr.zoom_ymin);
+         ymax = Math.max(ymax, gr.zoom_ymax);
+      }
+
+     if (gr?.zoom_zmin !== gr?.zoom_zmax) {
+         zmin = Math.min(zmin, gr.zoom_zmin);
+         zmax = Math.max(zmax, gr.zoom_zmax);
+      }
+
+      const ensureBins = (nx, ny) => {
+         if (hist.fNcells !== (nx + 2) * (ny + 2)) {
+            hist.fNcells = (nx + 2) * (ny + 2);
+            hist.fArray = new Float32Array(hist.fNcells);
+         }
+         hist.fArray.fill(0);
+         hist.fXaxis.fNbins = nx;
+         hist.fXaxis.fXbins = [];
+         hist.fYaxis.fNbins = ny;
+         hist.fYaxis.fXbins = [];
+         hist.fXaxis.fXmin = xmin;
+         hist.fXaxis.fXmax = xmax;
+         hist.fYaxis.fXmin = ymin;
+         hist.fYaxis.fXmax = ymax;
+         hist.fMinimum = zmin;
+         hist.fMaximum = zmax;
+      };
+
+      delete this._fail_eval;
+
+      if (!this._use_saved_points) {
+         const npx = Math.max(func.fNpx, 20),
+               npy = Math.max(func.fNpy, 20),
+               npz = Math.max(func.fNpz, 20);
+         let iserror = false;
+
+         if (!func.evalPar && !proivdeEvalPar(func))
+            iserror = true;
+
+         ensureBins(npx, npy);
+
+         if (logx)
+            produceTAxisLogScale(hist.fXaxis, npx, xmin, xmax);
+         if (logy)
+            produceTAxisLogScale(hist.fYaxis, npy, ymin, ymax);
+
+         const arrv = new Array(npz), arrz = new Array(npz);
+         for (let k = 0; k < npz; ++k)
+            arrz[k] = zmin + k / (npz - 1) * (zmax - zmin);
+
+         for (let j = 0; (j < npy) && !iserror; ++j) {
+            for (let i = 0; (i < npx) && !iserror; ++i) {
+               const x = hist.fXaxis.GetBinCenter(i+1),
+                     y = hist.fYaxis.GetBinCenter(j+1);
+               let z = 0;
+
+               try {
+                  for (let k = 0; k < npz; ++k)
+                     arrv[k] = func.evalPar(x, y, arrz[k]);
+
+                  z = findZValue(arrz, arrv);
+               } catch {
+                  iserror = true;
+               }
+
+               if (!iserror)
+                  hist.setBinContent(hist.getBin(i + 1, j + 1), Number.isFinite(z) ? z : 0);
+            }
+         }
+
+         if (iserror)
+            this._fail_eval = true;
+
+         if (iserror && (nsave > 0))
+            this._use_saved_points = true;
+      }
+
+      if (this._use_saved_points) {
+         xmin = func.fSave[nsave]; xmax = func.fSave[nsave+1];
+         ymin = func.fSave[nsave+2]; ymax = func.fSave[nsave+3];
+         zmin = func.fSave[nsave+4]; zmax = func.fSave[nsave+5];
+         const npx = Math.round(func.fSave[nsave+6]),
+               npy = Math.round(func.fSave[nsave+7]),
+               npz = Math.round(func.fSave[nsave+8]),
+               // dx = (xmax - xmin) / npx,
+               // dy = (ymax - ymin) / npy,
+               dz = (zmax - zmin) / npz;
+
+         ensureBins(npx + 1, npy + 1);
+
+         const arrv = new Array(npz + 1), arrz = new Array(npz + 1);
+         for (let k = 0; k <= npz; k++)
+            arrz[k] = zmin + k*dz;
+
+         for (let i = 0; i <= npx; ++i) {
+            for (let j = 0; j <= npy; ++j) {
+               for (let k = 0; k <= npz; k++)
+                  arrv[k] = func.fSave[i + (npx + 1)*(j + (npy + 1)*k)];
+               const z = findZValue(arrz, arrv);
+               hist.setBinContent(hist.getBin(i + 1, j + 1), Number.isFinite(z) ? z : 0);
+            }
+         }
+      }
+
+      hist.fName = 'Func';
+      setHistogramTitle(hist, func.fTitle);
+
+
+      // hist.fMinimum = func.fMinimum;
+      // hist.fMaximum = func.fMaximum;
+      // fHistogram->SetContour(fContour.fN, levels);
+      hist.fLineColor = func.fLineColor;
+      hist.fLineStyle = func.fLineStyle;
+      hist.fLineWidth = func.fLineWidth;
+      hist.fFillColor = func.fFillColor;
+      hist.fFillStyle = func.fFillStyle;
+      hist.fMarkerColor = func.fMarkerColor;
+      hist.fMarkerStyle = func.fMarkerStyle;
+      hist.fMarkerSize = func.fMarkerSize;
+      hist.fBits |= kNoStats;
+
+      return hist;
+   }
+
+   extractAxesProperties(ndim) {
+      super.extractAxesProperties(ndim);
+
+      const func = this.$func, nsave = func?.fSave.length ?? 0;
+
+      if (nsave > 9 && this._use_saved_points) {
+         this.xmin = Math.min(this.xmin, func.fSave[nsave-9]);
+         this.xmax = Math.max(this.xmax, func.fSave[nsave-8]);
+         this.ymin = Math.min(this.ymin, func.fSave[nsave-7]);
+         this.ymax = Math.max(this.ymax, func.fSave[nsave-6]);
+         this.zmin = Math.min(this.zmin, func.fSave[nsave-5]);
+         this.zmax = Math.max(this.zmax, func.fSave[nsave-4]);
+      }
+      if (func) {
+         this.xmin = Math.min(this.xmin, func.fXmin);
+         this.xmax = Math.max(this.xmax, func.fXmax);
+         this.ymin = Math.min(this.ymin, func.fYmin);
+         this.ymax = Math.max(this.ymax, func.fYmax);
+         this.zmin = Math.min(this.zmin, func.fZmin);
+         this.zmax = Math.max(this.zmax, func.fZmax);
+      }
+   }
+
+   /** @summary retrurn tooltips for TF2 */
+   getTF2Tooltips(pnt) {
+      const lines = [this.getObjectHint()],
+            funcs = this.getFramePainter()?.getGrFuncs(this.options.second_x, this.options.second_y);
+
+      if (!funcs || !isFunc(this.$func?.evalPar)) {
+         lines.push('grx = ' + pnt.x, 'gry = ' + pnt.y);
+         return lines;
+      }
+
+      const x = funcs.revertAxis('x', pnt.x),
+            y = funcs.revertAxis('y', pnt.y);
+      let z = 0, iserror = false;
+
+       try {
+          z = this.$func.evalPar(x, y);
+       } catch {
+          iserror = true;
+       }
+
+      lines.push('x = ' + funcs.axisAsText('x', x),
+                 'y = ' + funcs.axisAsText('y', y),
+                 'value = ' + (iserror ? '<fail>' : floatToString(z, gStyle.fStatFormat)));
+      return lines;
+   }
+
+   /** @summary process tooltip event for TF2 object */
+   processTooltipEvent(pnt) {
+      if (this._use_saved_points)
+         return super.processTooltipEvent(pnt);
+
+      let ttrect = this.draw_g?.selectChild('.tooltip_bin');
+
+      if (!this.draw_g || !pnt) {
+         ttrect?.remove();
+         return null;
+      }
+
+      const res = { name: this.$func?.fName, title: this.$func?.fTitle,
+                  x: pnt.x, y: pnt.y,
+                  color1: this.lineatt?.color ?? 'green',
+                  color2: this.fillatt?.getFillColorAlt('blue') ?? 'blue',
+                  lines: this.getTF2Tooltips(pnt), exact: true, menu: true };
+
+      if (ttrect.empty()) {
+         ttrect = this.draw_g.append('svg:circle')
+                             .attr('class', 'tooltip_bin')
+                             .style('pointer-events', 'none')
+                             .style('fill', 'none')
+                             .attr('r', (this.lineatt?.width ?? 1) + 4);
+      }
+
+      ttrect.attr('cx', pnt.x)
+            .attr('cy', pnt.y)
+            .call(this.lineatt?.func);
+
+      return res;
+   }
+
+   /** @summary fill information for TWebCanvas
+     * @private */
+   fillWebObjectOptions(opt) {
+      // mark that saved points are used or evaluation failed
+      opt.fcust = this._fail_eval ? 'func_fail' : '';
+   }
+
+   /** @summary draw TF3 object */
+   static async draw(dom, tf3, opt) {
+      if (!isStr(opt)) opt = '';
+      let p = opt.indexOf(';webcanv_hist'), webcanv_hist = false, force_saved = false;
+      if (p >= 0) {
+         webcanv_hist = true;
+         opt = opt.slice(0, p);
+      }
+      p = opt.indexOf(';force_saved');
+      if (p >= 0) {
+         force_saved = true;
+         opt = opt.slice(0, p);
+      }
+
+      const d = new DrawOptions(opt);
+      if (d.empty())
+         opt = 'surf1';
+      else if (d.opt === 'SAME')
+         opt = 'surf1 same';
+
+      if ((opt.indexOf('same') === 0) || (opt.indexOf('SAME') === 0)) {
+         if (!getElementMainPainter(dom))
+            opt = 'A_ADJUST_FRAME_' + opt.slice(4);
+      }
+
+      let hist;
+
+      if (webcanv_hist) {
+         const dummy = new ObjectPainter(dom);
+         hist = dummy.getPadPainter()?.findInPrimitives('Func', clTH2F);
+      }
+
+      if (!hist) {
+         hist = createHistogram(clTH2F, 20, 20);
+         hist.fBits |= kNoStats;
+      }
+
+      const painter = new TF3Painter(dom, hist);
+
+      painter.$func = tf3;
+      painter.webcanv_hist = webcanv_hist;
+      painter.force_saved = force_saved;
+      painter.createTF3Histogram(tf3, hist);
+      return THistPainter._drawHist(painter, opt);
+   }
+
+} // class TF3Painter
+
+var TF3Painter$1 = /*#__PURE__*/Object.freeze({
+__proto__: null,
+TF3Painter: TF3Painter
 });
 
 /**
@@ -123865,6 +124218,7 @@ exports.clTCutG = clTCutG;
 exports.clTDiamond = clTDiamond;
 exports.clTF1 = clTF1;
 exports.clTF2 = clTF2;
+exports.clTF3 = clTF3;
 exports.clTFile = clTFile;
 exports.clTGaxis = clTGaxis;
 exports.clTGeoNode = clTGeoNode;
