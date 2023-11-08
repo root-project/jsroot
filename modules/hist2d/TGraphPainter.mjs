ff08f23dec8187cc81e9c830b41b2d21b22b6777
Sergey Linev <S.Linev@gsi.de>
2023-11-08 15:13:38 +0100

    Provide needDrawFunc for TGraphPainter
    It also has kNoStats and it also may disable TF1 drawing

--- modules/hist2d/TGraphPainter.mjs
+++ modules/hist2d/TGraphPainter.mjs
@@ -409,6 +409,17 @@ class TGraphPainter extends ObjectPainter {
       return optbins;
    }
 
+   /** @summary Check if such function should be drawn directly */
+   needDrawFunc(graph, func) {
+      if (func._typename === clTPaveStats)
+          return (func.fName !== 'stats') || !graph.TestBit(kNoStats); // kNoStats is same for graph and histogram
+
+       if ((func._typename === clTF1) || (func._typename === clTF2))
+          return !func.TestBit(BIT(9)); // TF1::kNotDraw
+
+       return true;
+   }
+
    /** @summary Returns tooltip for specified bin */
    getTooltips(d) {
       const pmain = this.get_main(), lines = [],
