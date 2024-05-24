7dd04837d934ccf489bfc471752aa5a0770da8f9
Sergey Linev <S.Linev@gsi.de>
2024-05-24 10:57:36 +0200

    Fix - TF1 zoom selection

--- modules/hist/TF1Painter.mjs
+++ modules/hist/TF1Painter.mjs
@@ -158,13 +158,14 @@ class TF1Painter extends TH1Painter {
             pad = this.getPadPainter()?.getRootPad(true),
             logx = pad?.fLogx,
             gr = fp?.getGrFuncs(this.second_x, this.second_y);
-      let xmin = tf1.fXmin, xmax = tf1.fXmax;
+      let xmin = tf1.fXmin, xmax = tf1.fXmax, np = Math.max(tf1.fNpx, 100);
 
       if (gr?.zoom_xmin !== gr?.zoom_xmax) {
+         const dx = (xmax - xmin) / np;
          if ((xmin < gr.zoom_xmin) && (gr.zoom_xmin < xmax))
-            xmin = gr.zoom_xmin;
+            xmin = Math.max(xmin, gr.zoom_xmin - dx);
          if ((xmin < gr.zoom_xmax) && (gr.zoom_xmax < xmax))
-            xmax = gr.zoom_xmax;
+            xmax = Math.min(xmax, gr.zoom_xmax + dx);
       }
 
       this._use_saved_points = (tf1.fSave.length > 3) && (settings.PreferSavedPoints || this.force_saved);
@@ -182,7 +183,6 @@ class TF1Painter extends TH1Painter {
       delete this._fail_eval;
 
       if (!this._use_saved_points) {
-         const np = Math.max(tf1.fNpx, 100);
          let iserror = false;
 
          if (!tf1.evalPar) {
@@ -226,9 +226,10 @@ class TF1Painter extends TH1Painter {
       // in the case there were points have saved and we cannot calculate function
       // if we don't have the user's function
       if (this._use_saved_points) {
-         let np = tf1.fSave.length - 2, custom_xaxis = null;
+         np = tf1.fSave.length - 2;
          xmin = tf1.fSave[np];
          xmax = tf1.fSave[np + 1];
+         let custom_xaxis = null
 
          if (xmin === xmax) {
             xmin = tf1.fSave[--np];
