f9e8692217781801f725ceeb1282a6f392c6c76b
Sergey Linev <S.Linev@gsi.de>
2024-08-30 12:39:38 +0200

    Adjust alice_esd demo
    Use module
    Use explicit name

--- demo/alice_esd.mjs
+++ demo/alice_esd.mjs
@@ -3,18 +3,20 @@
 // geometry in https://root.cern/files/alice_ESDgeometry.root
 // Have to return Promise with list of objects which can be drawn on geometry
 
-async function extract_geo_tracks(tree, opt) {
-   // as first argument, tree should be provided
+import { create, version, TSelector, treeProcess  } from 'jsroot';
 
-   console.log('CALL async function extract_geo_tracks');
+console.log(`LOAD alice_esd.mjs with JSROOT ${version}`);
 
-   let handle = await import(JSROOT.source_dir + 'modules/tree.mjs');
+/** Name of the function 'extract_tracks' is specified as drop parameter in the URL string
+ * As first argument of the function TTree instance is provided */
+globalThis.extract_tracks = async function(tree, opt) {
+   console.log('CALL extract_tracks');
 
-   const selector = new handle.TSelector();
+   const selector = new TSelector();
 
    selector.addBranch('ESDfriend.fTracks.fPoints', 'pnts');
 
-   let lst = JSROOT.create('TList'), numentry = 0, numtracks = 0;
+   let lst = create('TList'), numentry = 0, numtracks = 0;
 
    selector.Process = function() {
       // function called for every entry
@@ -28,7 +30,7 @@ async function extract_geo_tracks(tree, opt) {
          numtracks++;
          const arr = pnts[p];
          if (!arr.fNPoints) continue;
-         const track = JSROOT.create('TGeoTrack');
+         const track = create('TGeoTrack');
          track.fNpoints = arr.fNPoints*4;
          track.fPoints = new Float32Array(track.fNpoints*4);
          for (let k = 0; k < arr.fNPoints; ++k) {
@@ -39,7 +41,8 @@ async function extract_geo_tracks(tree, opt) {
          track.fLineWidth = 2;
          track.fLineColor = 3;
          lst.Add(track);
-         if (numtracks > 100) return this.Abort(); // do not accumulate too many tracks
+         if (numtracks > 100)
+            return this.Abort(); // do not accumulate too many tracks
       }
    }
 
@@ -48,11 +51,8 @@ async function extract_geo_tracks(tree, opt) {
       console.log(`Read done num entries ${numentry} tracks ${numtracks}`);
    }
 
-   await handle.treeProcess(tree, selector);
-
-   console.log('FINISH extract_geo_tracks');
-
-   return lst;
+   return treeProcess(tree, selector).then( () => {
+      console.log('FINISH extract_tracks');
+      return lst;
+   });
 }
-
-console.log(`LOAD alice_esd.js with JSROOT ${JSROOT.version}`);
