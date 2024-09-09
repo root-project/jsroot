e0067ef628a762bc01a83abd82255cd5fadc542d
Sergey Linev <S.Linev@gsi.de>
2024-09-09 13:07:23 +0200

    Adjust eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,37 +8,37 @@ export default [
     rules: {
         semi: "warn",
         quotes: ["warn", "single"],
-        "object-curly-spacing": "off",
-        "indent": "off",
-        "camelcase": "off",
-        "space-before-function-paren": ["warn", "never"],
-        "comma-spacing": "warn",
-        "keyword-spacing": "warn",
+        indent: "off",
+        camelcase: "off",
         "prefer-const": "warn",
-        "eqeqeq": "warn",
-        "spaced-comment": "warn",
+        eqeqeq: "warn",
         "array-bracket-spacing": "warn",
-        "key-spacing": "warn",
-        "space-in-parens": "warn",
-        "computed-property-spacing": "warn",
-        "semi-spacing": "warn",
-        "no-floating-decimal": "warn",
-        "semi": "warn",
         "object-curly-newline": ["warn", {
           "ObjectExpression": { "consistent": true },
           "ObjectPattern": { "consistent": true },
           "ImportDeclaration": "never",
           "ExportDeclaration": "never"
         }],
-        "curly": ["warn", "multi-or-nest"],
+        curly: ["warn", "multi-or-nest"],
         "one-var": ["warn", "consecutive"],
         "space-infix-ops": "off",
         "no-multi-spaces": ["warn", { ignoreEOLComments: true }],
         "no-multiple-empty-lines": "off",
         "object-property-newline": "off",
-        // "promise/param-names": ["warn", { resolvePattern: "^resolve*", rejectPattern: "^reject*" } ],
         "no-new-func": "off",
-        "padded-blocks": ["warn", { "blocks": "never", "classes": "always", "switches": "never" }]
+        "padded-blocks": ["warn", { "blocks": "never", "classes": "always", "switches": "never" }],
+
+        // deprecated, no idea how to replace
+        // "object-curly-spacing": "off",
+        "keyword-spacing": "warn",
+        "semi-spacing": "warn",
+        "no-floating-decimal": "warn",
+        "space-in-parens": "warn",
+        "comma-spacing": "warn",
+        "key-spacing": "warn",
+        "spaced-comment": "warn",
+        "computed-property-spacing": "warn",
+        "space-before-function-paren": ["warn", "never"]
     }
   }
 ];
