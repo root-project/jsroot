0a72d4a1f95eead0c2b89aa2dd88fc917fc3bae9
Sergey Linev <S.Linev@gsi.de>
2024-10-02 17:06:34 +0200

    Fix - `#bf{#bf{Unbold}}` must be unbold
    Same with italic

--- modules/base/latex.mjs
+++ modules/base/latex.mjs
@@ -522,7 +522,7 @@ function parseLatex(node, arg, label, curr) {
    },
 
    createSubPos = fscale => {
-      return { lvl: curr.lvl + 1, x: 0, y: 0, fsize: curr.fsize*(fscale || 1), color: curr.color, font: curr.font, parent: curr, painter: curr.painter };
+      return { lvl: curr.lvl + 1, x: 0, y: 0, fsize: curr.fsize*(fscale || 1), color: curr.color, font: curr.font, parent: curr, painter: curr.painter, bold: curr.bold, italic: curr.italic };
    };
 
    while (label) {
