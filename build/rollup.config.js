2d444df8753df10a43436f5dcee3a1287eafdc8d
Sergey Linev <S.Linev@gsi.de>
2024-10-29 15:58:27 +0100

    Eliminate r162 revision of three.js - use from npm

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -6,12 +6,9 @@ import ascii from 'rollup-plugin-ascii';
 import ignore from 'rollup-plugin-ignore';
 import meta from '../package.json' assert { type: 'json' };
 
-const ignore_jsroot_modules = [
-  './base/lzma.mjs', './base/zstd.mjs', './base/zstd.mjs',
-  '../r162/three.mjs',  '../r162/three_addons.mjs'
-];
+const ignore_jsroot_modules = [ './base/lzma.mjs', './base/zstd.mjs', './base/zstd.mjs' ];
 
-const external_node_modules = ['mathjax', 'jsdom', 'fs', 'canvas', 'tmp', 'zlib', 'xhr2', '@oneidentity/zstd-js', 'gl'];
+const external_node_modules = ['mathjax', 'jsdom', 'fs', 'canvas', 'tmp', 'zlib', 'xhr2', '@oneidentity/zstd-js', 'gl', 'three', 'three/addons'];
 
 // TODO: maybe keep node modules as external to be able use produced builds as well?
 const ignore_modules = ignore_jsroot_modules.concat(external_node_modules);
