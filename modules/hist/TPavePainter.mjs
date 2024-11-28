22264813850103d3f2c0a0588f9b24e9f134a649
Sergey Linev <S.Linev@gsi.de>
2024-11-28 17:53:52 +0100

    Use ticks size from Zaxis in palette

--- modules/hist/TPavePainter.mjs
+++ modules/hist/TPavePainter.mjs
@@ -893,11 +893,12 @@ class TPavePainter extends ObjectPainter {
 
       this._palette_vertical = (palette.fX2NDC - palette.fX1NDC) < (palette.fY2NDC - palette.fY1NDC);
 
-      axis.fTickSize = 0.6 * s_width / width; // adjust axis ticks size
+      axis.fTickSize = 0.03; // adjust axis ticks size
 
       if ((typeof zaxis?.fLabelOffset !== 'undefined') && !is_th3) {
          axis.fBits = zaxis.fBits & ~EAxisBits.kTickMinus & ~EAxisBits.kTickPlus;
          axis.fTitle = zaxis.fTitle;
+         axis.fTickSize = zaxis.fTickLength;
          axis.fTitleSize = zaxis.fTitleSize;
          axis.fTitleOffset = zaxis.fTitleOffset;
          axis.fTextColor = zaxis.fTitleColor;
@@ -938,13 +939,13 @@ class TPavePainter extends ObjectPainter {
 
       if (this._palette_vertical) {
          this._swap_side = palette.fX2NDC < 0.5;
-         axis.fChopt = '+' + (this._swap_side ? 'R' : 'L'); // clearly configure text align
+         axis.fChopt = 'S+' + (this._swap_side ? 'R' : 'L'); // clearly configure text align
          this.z_handle.configureAxis('zaxis', gzmin, gzmax, zmin, zmax, true, [0, s_height], { log, fixed_ticks: cjust ? levels : null, maxTickSize: Math.round(s_width*sizek), swap_side: this._swap_side, minposbin: main.gminposbin });
          axis_transform = this._swap_side ? null : `translate(${s_width})`;
          if (pad?.fTickz) axis_second = this._swap_side ? s_width : -s_width;
       } else {
          this._swap_side = palette.fY1NDC > 0.5;
-         axis.fChopt = '+';
+         axis.fChopt = 'S+';
          this.z_handle.configureAxis('zaxis', gzmin, gzmax, zmin, zmax, false, [0, s_width], { log, fixed_ticks: cjust ? levels : null, maxTickSize: Math.round(s_height*sizek), swap_side: this._swap_side, minposbin: main.gminposbin });
          axis_transform = this._swap_side ? null : `translate(0,${s_height})`;
          if (pad?.fTickz) axis_second = this._swap_side ? s_height : -s_height;
