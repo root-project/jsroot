1fdc885d1ba7068c6531ee8c4d6b7779f8991316
Sergey Linev <S.Linev@gsi.de>
2025-04-09 13:22:08 +0200

    Always use drawopt for graph
    In default case it should be copied to args

--- modules/tree.mjs
+++ modules/tree.mjs
@@ -932,7 +932,7 @@ class TDrawSelector extends TSelector {
       if (args.drawopt !== undefined)
          this.drawopt = args.drawopt;
       else
-         this.drawopt = this.graph ? 'P' : '';
+         args.drawopt = this.drawopt = this.graph ? 'P' : '';
 
       return true;
    }
