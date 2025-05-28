c50339661dac29afac732f762c7fa4c64425c1b0
Sergey Linev <S.Linev@gsi.de>
2025-05-28 11:28:23 +0200

    Add RNTuple unziping of the header/footer blobs
    Use existing R__unzip function from io.mjs -
    seems to be zip buffer is identical

--- modules/rntuple.mjs
+++ modules/rntuple.mjs
@@ -1,21 +1,58 @@
+import { R__unzip } from './io.mjs';
+
+
+class RNTupleDescriptorBuilder {
+
+   deserializeHeader(header_blob) {
+      if (!header_blob)
+         return;
+
+      this.xxhash3 = 1234;
+      this.featuresFlags = [];
+      this.name = 'rntuple';
+      this.description = 'description';
+      // this.deserializeSchemaDescription ...
+   }
+
+   deserializeFooter(footer_blob) {
+      if (!footer_blob)
+         return;
+      this.xxhash3 = 1234;
+   }
+
+}
+
 /** @summary Very preliminary function to read header/footer from RNTuple
   * @private */
 async function readHeaderFooter(tuple) {
    if (!tuple.$file)
       return false;
 
-   console.log('reading from RNtuple ', tuple.fSeekHeader, tuple.fNBytesHeader, tuple.fSeekFooter, tuple.fNBytesFooter);
-
-   // request two blobs from file
+   // request header and footer buffers from the file
    return tuple.$file.readBuffer([tuple.fSeekHeader, tuple.fNBytesHeader, tuple.fSeekFooter, tuple.fNBytesFooter]).then(blobs => {
-
       if (blobs?.length !== 2)
          return false;
 
-      const header_blob = blobs[0],
-            footer_blob = blobs[1];
+      // unzip both buffers
+      return Promise.all([
+         R__unzip(blobs[0], tuple.fLenHeader),
+         R__unzip(blobs[1], tuple.fLenFooter)
+      ]).then(unzip_blobs => {
+         const header_blob = unzip_blobs[0],
+               footer_blob = unzip_blobs[1];
+         if (!header_blob || !footer_blob)
+            return false;
 
-      return header_blob && footer_blob;
+         // create builder description and decode it - dummy for the moment
+
+         tuple.builder = new RNTupleDescriptorBuilder;
+
+         tuple.builder.deserializeHeader(header_blob);
+
+         tuple.builder.deserializeFooter(footer_blob);
+
+         return true;
+      });
    });
 }
 
@@ -24,15 +61,15 @@ async function readHeaderFooter(tuple) {
   * @desc Used by hierarchy painter to explore sub-elements
   * @private */
 async function tupleHierarchy(tuple_node, tuple) {
-
    tuple_node._childs = [];
-   tuple_node._tuple = tuple;  // set reference, will be used later by TTree::Draw
+   // tuple_node._tuple = tuple;  // set reference, will be used later by RNTuple::Draw
 
    return readHeaderFooter(tuple).then(res => {
       if (!res)
          return res;
 
       // just show which objects belongs to hierarchy
+      // one need to fill list of items from tuple.builder ... object
       for (let k = 0; k < 3; ++k) {
          tuple_node._childs.push({
             _name: `dummy${k}`,
@@ -43,7 +80,7 @@ async function tupleHierarchy(tuple_node, tuple) {
       }
 
       return true;
-   })
+   });
 }
 
 export { tupleHierarchy };
