396f63f0e85ca678634ce0ef696f781602162709
Krmjn09 <kmahajan1_be22@thapar.edu>
2025-06-05 13:35:26 +0530

    Add RBufferReader class, rntuple.mjs file, and added in workflow

---
+++ demo/node/RNTuple_reader.js
@@ -0,0 +1,76 @@
+const LITTLE_ENDIAN = true;
+
+class RBufferReader {
+  constructor(buffer) {
+    this.buffer = buffer;
+    this.view = new DataView(buffer);
+    this.offset = 0;
+  }
+
+  // Moving to a specific position in the buffer
+  seek(position) {
+    this.offset = position;
+  }
+
+  // Read unsigned 8-bit integer (1 BYTE)
+  readU8() {
+    const val = this.view.getUint8(this.offset, LITTLE_ENDIAN);
+    this.offset += 1;
+    return val;
+  }
+
+  // Read unsigned 16-bit integer (2 BYTES)
+  readU16() {
+    const val = this.view.getUint16(this.offset, LITTLE_ENDIAN)
+    this.offset += 2;
+    return val;
+  }
+
+  // Read unsigned 32-bit integer (4 BYTES)
+  readU32() {
+    const val = this.view.getUint32(this.offset, LITTLE_ENDIAN)
+    this.offset += 4;
+    return val;
+  }
+
+  // Read signed 8-bit integer (1 BYTE)
+  readS8() {
+    const val = this.view.getInt8(this.offset, LITTLE_ENDIAN)
+    this.offset += 1;
+    return val;
+  }
+
+  // Read signed 16-bit integer (2 BYTES)
+  readS16() {
+    const val = this.view.getInt16(this.offset, LITTLE_ENDIAN)
+    this.offset += 2;
+    return val;
+  }
+
+  // Read signed 32-bit integer (4 BYTES)
+  readS32() {
+    const val = this.view.getInt32(this.offset, LITTLE_ENDIAN);
+    this.offset += 4;
+    return val;
+  }
+
+  // Read 32-bit float (4 BYTES)
+  readF32() {
+    const val = this.view.getFloat32(this.offset, LITTLE_ENDIAN);
+    this.offset += 4;
+    return val;
+  }
+
+  // Read a string with 32-bit length prefix
+  readString() {
+    // Reading the length
+    const length = this.readU32();
+    // Reading the payload
+    let str = '';
+    for (let i = 0; i < length; i++) 
+      str += String.fromCharCode(this.readU8());
+    return str;
+  }
+}
+
+export { RBufferReader };
