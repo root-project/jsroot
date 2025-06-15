dbe70c1215314385160d3e208f029c948b545f20
Krmjn09 <mahajankriti672@gmail.com>
2025-06-15 19:46:20 +0530

    Added Field Descriptor logic

--- demo/node/rntuple_test.js
+++ demo/node/rntuple_test.js
@@ -18,11 +18,6 @@ if (rntuple.builder?.name !== 'Staff')
 else
   console.log('OK: name is', rntuple.builder?.name);
 
-if (typeof rntuple.builder?.version !== 'number')
-  console.error('FAILURE: version is missing or invalid');
-else
-  console.log('OK: version is', rntuple.builder.version);
-
 if (!rntuple.builder?.description)
   console.error('FAILURE: description is missing');
 else
@@ -32,4 +27,19 @@ if (rntuple.builder?.xxhash3 === undefined || rntuple.builder.xxhash3 === null)
   console.warn('WARNING: xxhash3 is missing');
 else
   console.log('OK: xxhash3 is', '0x' + rntuple.builder.xxhash3.toString(16).padStart(16, '0'));
+
+// Fields Check
+
+if (!rntuple.builder?.fieldDescriptors?.length)
+  console.error('FAILURE: No fields deserialized');
+else {
+  console.log(`OK: ${rntuple.builder.fieldDescriptors.length} field(s) deserialized`);
+  for (let i = 0; i < rntuple.builder.fieldDescriptors.length; ++i) {
+    const field = rntuple.builder.fieldDescriptors[i];
+    if (!field.fieldName || !field.typeName)
+      console.error(`FAILURE: Field ${i} is missing name or type`);
+    else
+      console.log(`OK: Field ${i}: ${field.fieldName} (${field.typeName})`);
+  }
+}
 
