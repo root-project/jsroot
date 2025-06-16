2821e7a6cb6c7ca8c141e727a11892226fb9add7
Sergey Linev <S.Linev@gsi.de>
2025-06-16 14:48:07 +0200

    Allow draw if 'inspect' specified as the only option

--- modules/draw.mjs
+++ modules/draw.mjs
@@ -136,7 +136,7 @@ drawFuncs = { lst: [
    { name: /^TBranch/, icon: 'img_branch', draw: () => import_tree().then(h => h.drawTree), dflt: 'expand', opt: ';dump', ctrl: 'dump', shift: kInspect, ignore_online: true, always_draw: true },
    { name: /^TLeaf/, icon: 'img_leaf', noexpand: true, draw: () => import_tree().then(h => h.drawTree), opt: ';dump', ctrl: 'dump', ignore_online: true, always_draw: true },
    { name: 'ROOT::RNTuple', icon: 'img_tree', get_expand: () => import('./rntuple.mjs').then(h => h.tupleHierarchy) },
-   { name: 'ROOT::RNTupleField', icon: 'img_leaf' },
+   { name: 'ROOT::RNTupleField', icon: 'img_leaf', opt: 'inspect', ctrl: 'inspect' },
    { name: clTList, icon: 'img_list', draw: () => import_h().then(h => h.drawList), get_expand: () => import_h().then(h => h.listHierarchy), dflt: 'expand' },
    { name: clTHashList, sameas: clTList },
    { name: clTObjArray, sameas: clTList },
@@ -250,8 +250,9 @@ function getDrawHandle(kind, selector) {
 function canDrawHandle(h) {
    if (isStr(h))
       h = getDrawHandle(h);
-   if (!isObject(h)) return false;
-   return h.func || h.class || h.draw || h.draw_field;
+   if (!isObject(h))
+      return false;
+   return (h.func || h.class || h.draw || h.draw_field || h.opt === 'inspect');
 }
 
 /** @summary Provide draw settings for specified class or kind
@@ -419,6 +420,8 @@ async function draw(dom, obj, opt) {
       return performDraw();
 
    let promise;
+
+   console.log('here', handle);
 
    if (isFunc(handle.class)) {
       // class coded as async function which returns class handle
