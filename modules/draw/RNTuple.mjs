29fc1a104ab40cd07b991a9db8ec5df2509abb1a
Sergey Linev <S.Linev@gsi.de>
2025-07-30 10:09:15 +0200

    Introduce RNTuple draw function based on TDrawSelector

---
+++ modules/draw/RNTuple.mjs
@@ -0,0 +1,31 @@
+import { treeDrawProgress } from './TTree.mjs';
+import { rntupleDraw } from '../rntuple.mjs';
+
+/** @summary function called from draw()
+  * @desc just envelope for real TTree::Draw method which do the main job
+  * Can be also used for the branch and leaf object
+  * @private */
+async function drawRNTuple(dom, obj, opt) {
+
+   console.log('drawRNTuple');
+   let tuple = null, args = { };
+
+   if (obj?.$tuple) {
+      // case of fictional ROOT::RNTupleField
+      tuple = obj.$tuple;
+      args.expr = obj._name;
+   }
+
+   if (!tuple)
+      throw Error('No RNTuple object available for drawing');
+
+   console.log('expression', args.expr);
+
+   args.drawid = dom;
+
+   args.progress = treeDrawProgress.bind(args);
+
+   return rntupleDraw(tuple, args).then(res => args.progress(res, true));
+}
+
+export { drawRNTuple };
