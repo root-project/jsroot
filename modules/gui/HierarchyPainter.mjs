0e73e2c2950f97abcf780a3f1567a135ebab9377
Sergey Linev <S.Linev@gsi.de>
2025-11-25 10:49:36 +0100

    Create batch display when no frame id is provided

--- modules/gui/HierarchyPainter.mjs
+++ modules/gui/HierarchyPainter.mjs
@@ -3609,7 +3609,7 @@ class HierarchyPainter extends BasePainter {
          delete this.disp;
       }
 
-      if (this.disp_kind === 'batch') {
+      if ((this.disp_kind === 'batch') || !this.disp_frameid || isNodeJs()) {
          const pr = isNodeJs() ? _loadJSDOM() : Promise.resolve(null);
          return pr.then(handle => {
             this.disp = new BatchDisplay(1200, 800, handle?.body);
