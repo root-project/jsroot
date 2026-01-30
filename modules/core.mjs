d805d8e26ea6472202cca393f367f7aa46502512
Sergey Linev <S.Linev@gsi.de>
2026-01-30 12:49:41 +0100

    Build with bigint fix

--- modules/core.mjs
+++ modules/core.mjs
@@ -6,7 +6,7 @@ const version_id = '7.10.x',
 
 /** @summary version date
   * @desc Release date in format day/month/year like '14/04/2022' */
-version_date = '28/01/2026',
+version_date = '30/01/2026',
 
 /** @summary version id and date
   * @desc Produced by concatenation of {@link version_id} and {@link version_date}
