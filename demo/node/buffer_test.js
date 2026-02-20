38a5870a73b35097ae9584e63e54b335bf745838
Sergey Linev <S.Linev@gsi.de>
2026-02-20 12:43:21 +0100

    Fix eslint warnings in buffer_test.jd

--- demo/node/buffer_test.js
+++ demo/node/buffer_test.js
@@ -1,18 +1,18 @@
 import { RBufferReader } from 'jsroot/rntuple';
 
 // This is a test script
-//TEST 1
+// TEST 1
 {
    console.log('Perform basic RBufferReader test');
    // creating a buffer with one 32-bit unsigned integer
-   let buffer = new ArrayBuffer(4);
-   let view = new DataView(buffer);
+   const buffer = new ArrayBuffer(4),
+         view = new DataView(buffer);
 
-   view.setUint32(0, 0x12345678, true)
+   view.setUint32(0, 0x12345678, true);
 
    // read the buffer
-   let reader = new RBufferReader(buffer);
-   let value = reader.readU32();
+   const reader = new RBufferReader(buffer),
+         value = reader.readU32();
 
    if (value === 0x12345678)
       console.log('test 1 passed');
@@ -22,40 +22,39 @@ import { RBufferReader } from 'jsroot/rntuple';
 
 // TEST 2: Read 16-bit and 8-bit integers
 {
-   const buffer = new ArrayBuffer(3);
-   const view = new DataView(buffer);
+   const buffer = new ArrayBuffer(3),
+         view = new DataView(buffer);
    view.setUint16(0, 0xABCD, true);  // First 2 bytes
    view.setUint8(2, 0xEF);           // Last byte
-   const reader = new RBufferReader(buffer);
-   const u16 = reader.readU16();
-   const u8 = reader.readU8();
+   const reader = new RBufferReader(buffer),
+         u16 = reader.readU16(),
+         u8 = reader.readU8();
 
    if (u16 === 0xABCD)
       console.log('test - 2 - readU16 passed');
    else
-      console.error('FAILURE: test - 2 - readU16 does not match')
+      console.error('FAILURE: test - 2 - readU16 does not match');
 
    if (u8 === 0xEF)
       console.log('test- 2 - readU8 passed');
    else
-      console.error('FAILURE: test - 2 - readU8 does not match')
-  }
+      console.error('FAILURE: test - 2 - readU8 does not match');
+}
 
-//Test 3: Read a string
+// Test 3: Read a string
 {
-   const text = 'HELLO';
-   const buffer = new ArrayBuffer(4 + text.length) // 4 bytes for length and 5 for hello
-
-   const view = new DataView(buffer)
+   const text = 'HELLO',
+         buffer = new ArrayBuffer(4 + text.length), // 4 bytes for length and 5 for hello
+         view = new DataView(buffer);
 
    // set the 32 bit length of the string
    view.setUint32(0,text.length,true);
-   //set each char's ASCII code
+   // set each char's ASCII code
    for (let i = 0; i < text.length; i++)
-      view.setUint8(4+i,text.charCodeAt(i));
+      view.setUint8(4 + i, text.charCodeAt(i));
 
-   const reader = new RBufferReader(buffer);
-   const result = reader.readString();
+   const reader = new RBufferReader(buffer),
+         result = reader.readString();
 
    if (result === text)
       console.log('test 3 - readString passed');
